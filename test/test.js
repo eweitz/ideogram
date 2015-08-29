b50f72aa808b65d9784234570a67b26ed68a1ccc
Eric Weitz <eric.m.weitz@gmail.com>
2015-08-28 23:52:22 -0400

    Added test for writing initial SVG element to DOM

--- test/test.js
+++ test/test.js
@@ -17,26 +17,46 @@ describe("Ideogram", function() {
 
   var ideogram = new Ideogram(config);
 
+
   assert.equal(ideogram.config.container, ".small-ideogram");
 
   });  
 
-it("should have chromosome objects", function() {
+it("should write SVG to DOM", function() {
 
   var config = {
-    container: ".small-ideogram",
     taxid: 9606,
     chromosomes: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"],
     resolution: 550,
     chrWidth: 10,
     chrHeight: 150,
     chrMargin: 10,
-    rows: 2,
     showChromosomeLabels: true,
     orientation: "vertical"
   };
 
   var ideogram = new Ideogram(config);
+
+  var svg = d3.selectAll("svg")[0].length;
+  assert.equal(svg, 1);
+});
+
+it("should have chromosome objects", function() {
+
+  var config = {
+    taxid: 9606,
+    chromosomes: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"],
+    resolution: 550,
+    chrWidth: 10,
+    chrHeight: 150,
+    chrMargin: 10,
+    showChromosomeLabels: true,
+    orientation: "vertical"
+  };
+
+  var ideogram = new Ideogram(config);
+  console.log("ideogram:");
+  console.log(ideogram);
 
   console.log('ideogram.chromosomes');
   console.log(ideogram.chromosomes);
