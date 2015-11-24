1aeb1ffa038efd46cb83284db889e6510156cca5
Eric Weitz <eric.m.weitz@gmail.com>
2015-11-23 23:05:23 -0500

    Added CLI-specific test

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
+var mocha = require('gulp-mocha');
 
 gulp.task('default', function () {
-  return gulp
-  .src('test/runner.html')
-  .pipe(mochaPhantomJS({reporter: 'spec', dump:'test.log'}));
+
+  gulp
+    .src('test/web-runner.html')
+    .pipe(mochaPhantomJS({reporter: 'spec', dump:'test.log'}))
+
+  gulp.src('test/cli-test.js', {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it
+    .pipe(mocha({reporter: 'spec'}));
+
 });
---
+++ test/cli-test.js
@@ -0,0 +1,32 @@
+var assert = require('chai').assert;
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+describe("Ideogram CLI", function() {
+
+  it("should run the built-in example", function(done) {
+
+    this.timeout(10000);
+    var numImages = 100;
+
+    var cmd =
+    'ideogram ' +
+      '--taxid 9606 ' +
+      '--chromosomes 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,X,Y ' +
+      '--chr-width 10 ' +
+      '--chr-height 500 ' +
+      '--chr-margin 10 ' +
+      '--show-band-labels true ' +
+      '--show-chromosome-labels true ' +
+      '--orientation horizontal ' +
+      '--local-annotations-path data/annotations/' + numImages + '_virtual_snvs.json';
+
+    exec(cmd, function(error, stdout, stderr) {
+      var images = fs.readdirSync('images');
+      assert.equal(numImages, images.length);
+      done();
+    })
+
+  });
+
+});
