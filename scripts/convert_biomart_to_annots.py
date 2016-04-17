3a146e9f152f1bbfd3410b4b3920f263768ebed4
Eric Weitz <eric.m.weitz@gmail.com>
2016-04-17 15:11:28 -0400

    Adjust mock data

--- scripts/convert_biomart_to_annots.py
+++ scripts/convert_biomart_to_annots.py
@@ -42,14 +42,14 @@ for line in file[1:]:
 		gene_symbol,
 		start,
 		length,
-		random.randint(1,5),
+		random.randint(1,7),
 		random.randint(1,5),
 	]
 
 	annots[chr]["annots"].append(annot)
 
 top_annots = {}
-top_annots["keys"] = ["name", "start", "length", "tissue-type", "expression-level"]
+top_annots["keys"] = ["name", "start", "length", "expression-level", "gene-type"]
 top_annots["annots"] = annots
 annots = json.dumps(top_annots)
 
