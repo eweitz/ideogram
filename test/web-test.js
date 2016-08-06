e45a264cd76504cd5fac240606cb6d8685459474
Eric Weitz <eric.m.weitz@gmail.com>
2016-08-06 12:56:51 -0400

    Fix false-positive test failure

--- test/web-test.js
+++ test/web-test.js
@@ -82,6 +82,9 @@ describe("Ideogram", function() {
       done();
     }
 
+    // Clears default setting from beforeEach (test artifact)
+    delete config.organism;
+
     config.taxid = 10090;
     config.orientation = "horizontal";
     config.chromosomes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "X", "Y"];
