dea5c5bfd23665e7d31113b00f89596efb56aa9d
Eric Weitz <eric.m.weitz@gmail.com>
2016-09-10 18:16:42 -0400

    Add SNPedia results; add ClinVar review status, accessions, URLs; improve output format

--- scripts/convert_ancestrydna.py
+++ scripts/convert_ancestrydna.py
@@ -1,21 +1,69 @@
 import json, re
+from collections import OrderedDict
+
+show_snpedia_results = True
+
+data_dir = "data/analysis/"
+
+input_file = data_dir + "AncestryDNA.txt"
+output_file = data_dir + "genome_analysis.txt"
+
+# Raw sample data from AncestryDNA
+ancestrydna_sample =  open(input_file).readlines()
 
 # Example ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/archive/2016/clinvar_20160802.vcf.gz
-clinvar_vcf_file = open("data/annotations/clinvar_20160831.vcf").readlines()
+clinvar_vcf_file = open(data_dir + "clinvar_20160831.vcf").readlines()
+
+# From https://raw.githubusercontent.com/heiner/snpedia-23andme/master/snpedia-archive.json
+snpedia_json = json.loads(open(data_dir + "snpedia-archive.json").read())
+
+output = []
 
 rsids = {}
 
 clinallele_re = re.compile("CLNALLE=(-?\d+)")
 disease_re = re.compile("CLNDBN=([^;]*)")
 clinsig_re = re.compile("CLNSIG=([^;]*)")
+clinrevstat_re = re.compile("CLNREVSTAT=([^;]*)")
+clinacc_re = re.compile("CLNACC=([^;]*)")
 gene_re = re.compile("GENEINFO=(\w+)")
 
+num_ancestrydna_rsids = 0
 num_skipped_clinvars = 0
 
-# Column headers:
+annots = []
+clin_annots = []
+
+allele_map = {
+    "A": 0,
+    "T": 1,
+    "C": 2,
+    "G": 3,
+    "0": 4,
+    "I": 4, # indel / insertion
+    "D": 4 # indel / deletion
+}
+
+seen_chrs = {}
+seen_chrs_clin_annots = {}
+
+clinical_alleles = []
+
+clinvar_url = "https://www.ncbi.nlm.nih.gov/clinvar/"
+
+def complement(nt):
+    complements = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C"
+    }
+    return complements[nt]
+
+# Column headers of VCF file:
 # #CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO
 #
-# Example VCF data line from clinvar_vcf_file:
+# Example line from body of VCF file:
 # 1       169519049       rs6025  T       C       .       .       RS=6025;RSPOS=169519049;RV;dbSNPBuildID=52;SSR=0;SAO=1;VP=0x050168000a0504053f130101;GENEINFO=F5:2153;WGT=1;VC=SNV;PM;PMC;SLO;NSM;REF;ASP;VLD;HD;GNO;KGPhase1;KGPhase3;LSD;MTP;OM;CLNALLE=0,1;CLNHGVS=NC_000001.10:g.169519049T\x3d,NC_000001.10:g.169519049T>C;CLNSRC=OMIM_Allelic_Variant,PharmGKB_Clinical_Annotation|PharmGKB;CLNORIGIN=1,1;CLNSRCID=612309.0001,1183689558|1183689558;CLNSIG=5|255|255|255|5,6;CLNDSDB=MedGen|.|.|MedGen:OMIM:SNOMED_CT|MedGen:OMIM:ORPHA:SNOMED_CT,MedGen;CLNDSDBID=C2674152|.|.|C0000809:614389:102878001|C0015499:227400:326:4320005,CN236515;CLNDBN=Thrombophilia_due_to_factor_V_Leiden|Ischemic_stroke\x2c_susceptibility_to|Budd-Chiari_syndrome\x2c_susceptibility_to|Recurrent_abortion|Factor_V_deficiency,hormonal_contraceptives_for_systemic_use_response_-_Toxicity/ADR;CLNREVSTAT=no_criteria|no_criteria|no_criteria|no_criteria|single,exp;CLNACC=RCV000000674.2|RCV000000675.3|RCV000000676.2|RCV000023935.2|RCV000205002.3,RCV000211384.1;CAF=0.00599,0.994;COMMON=1
 #
 # See top of clinvar_vcf_file for description of inner INFO columns
@@ -33,6 +81,8 @@ for line in clinvar_vcf_file:
     clinallele_indexes = clinallele_re.search(info).group(1).split(",")
     diseases = disease_re.search(info).group(1).split(",")
     clinsigs = clinsig_re.search(info).group(1).split(",")
+    clinrevstats = clinrevstat_re.search(info).group(1).split(",")
+    clinaccs = clinacc_re.search(info).group(1).split(",")
 
     if clinallele_indexes[0] == "-1":
         num_skipped_clinvars += 1
@@ -63,48 +113,54 @@ for line in clinvar_vcf_file:
     # In other words, each RS ID can have multiple alleles, and each allele
     # can be associated multiple one of more diseases,
     # each of which has one clinical significance
-    for i, clinsig in enumerate(clinsigs):
-        for j, cs in enumerate(clinsig.split("|")):
-            d = diseases[i].split("|")[j]
-            tmp.append([int(cs), d])
+    for i, clinsig_list in enumerate(clinsigs):
+        for j, clinsig in enumerate(clinsig_list.split("|")):
+
+            disease = diseases[i].split("|")[j]
+            disease = disease.replace("_", " ")
+            # TODO: Properly decode non-Python-Unicode Unicode hex codes
+            disease = disease.replace("\\x2c", ",")
+
+            clinacc = clinaccs[i].split("|")[j]
+
+            clinrevstat = clinrevstats[i].split("|")[j]
+
+            tmp.append([int(clinsig), disease, clinrevstat, clinacc])
     clinsigs = tmp
 
     rsids[rsid] = {
         "clinalleles": clinalleles,
         "clinsigs": clinsigs,
         "gene": gene
-        #"clinrevstat": info[25]
     }
 
-print("Number of ClinVar variants:")
-print(len(rsids))
-
-file_name = "data/annotations/AncestryDNA.txt"
-
-ancestry_dna_sample =  open(file_name).readlines()
-
-annots = []
-clin_annots = []
-
-allele_map = {
-    "A": 0,
-    "T": 1,
-    "C": 2,
-    "G": 3,
-    "0": 4,
-    "I": 4, # indel / insertion
-    "D": 4 # indel / deletion
+clinsig_labels = {
+    0: "Uncertain significance",
+    1: "Not provided",
+    2: "Benign",
+    3: "Likely benign",
+    4: "Likely pathogenic",
+    5: "Pathogenic",
+    6: "Drug response",
+    7: "Histocompatibility",
+    255: "Other"
 }
 
-seen_chrs = {}
-seen_chrs_clin_annots = {}
+rs_summaries = OrderedDict([
+    ("pathogenic", []),
+    ("likely_pathogenic", []),
+    ("drug_response", [])
+])
 
-clinical_alleles = []
+if show_snpedia_results:
+    rs_summaries["snpedia"] = []
 
-for line in ancestry_dna_sample:
+for line in ancestrydna_sample:
 
     if line[0] == "#" or line[:4] == "rsid":
         continue
+
+    num_ancestrydna_rsids += 1
 
     columns = line.strip().split("\t")
 
@@ -127,29 +183,77 @@ for line in ancestry_dna_sample:
     if (allele1 == allele2):
         homozygous = 1
 
+    if homozygous == 1:
+        # Zygosity
+        zygo = "homozygous"
+    else:
+        zygo = "heterozygous"
+
+    genotype = name + "(" + allele1 + ";" + allele2 + ")"
+
     if name not in rsids:
         continue
 
     clinalleles  = rsids[name]["clinalleles"]
 
+    if name in snpedia_json and snpedia_json[name]:
+        # SNPedia RS object, e.g.
+        a1 = allele1
+        a2 = allele2
+
+        if a1 in (("I", "D", "0")) or a1 in (("I", "D", "0")):
+            # Skip insertions, deletions, or unknown
+            continue
+
+        sample_genotype = a1 + a2
+        srs = snpedia_json[name]
+        if srs["original_orientation"] == "minus":
+            sample_genotype = complement(a1) + complement(a2)
+        if sample_genotype in srs["genotypes"]:
+            sg = srs["genotypes"][sample_genotype]
+            if sg["comment"].lower() in ((
+                "", "common in clinvar", "common in complete genomics",
+                "common on affy axiom data", "normal", "common", "?", "none",
+                "normal risk", "average", "common/normal",
+                "normal (orientation reversed)"
+            )):
+                # Skip uninformative entries
+                continue
+
+            # SNPedia seems noisier than ClinVar, also much overlap.
+            if show_snpedia_results:
+                rs_summaries["snpedia"].append(
+                    "SNPedia result for " + genotype + ":\n" +
+                        "\t" + sg["comment"]
+                )
+
     for i, clinallele in enumerate(clinalleles):
         if name in rsids and clinallele in set((allele1, allele2)):
-            #print("clinical: " + name)
+            #output.append("clinical: " + name)
             # TODO: Hom vs. het clinsig
-            cs_d = rsids[name]["clinsigs"][i]
-            clinsig = cs_d[0]
-            disease = cs_d[1]
-            if clinsig in set((4,5)):
+            cs_d_crs_ca = rsids[name]["clinsigs"][i]
+            clinsig = cs_d_crs_ca[0]
+            disease = cs_d_crs_ca[1]
+            clinrevstat = cs_d_crs_ca[2]
+            clinacc = cs_d_crs_ca[3]
+            if clinsig > 3 and clinsig != 255:
                 clinical_alleles.append(
                     name + " "
                     "chr" + chr + ":" + str(start) + " " +
                     rsids[name]["gene"]
                 )
-                if homozygous == 1 and clinsig == 5:
-                    print(
-                        "Homozygous pathogenic: " + name + ", " +
-                        "disease: " + disease
-                    )
+
+                cs_label = clinsig_labels[clinsig]
+
+                rs_summary = (
+                    "\n" + cs_label + ", " + zygo + ": " + genotype + "\n" +
+                        "\tDisease: " + disease + "\n" +
+                        "\tReview status: " + clinrevstat + "\n" +
+                        "\tClinVar record: " + clinvar_url + clinacc
+                )
+                key = cs_label.lower().replace(" ", "_")
+                rs_summaries[key].append(rs_summary)
+
             if clinsig in set((1,2,3,4,5)):
                 #clinsig -= 1
                 clin_annot = [name, start, length, clinsig]
@@ -186,7 +290,7 @@ top_annots["keys"] = [
 ]
 top_annots["annots"] = annots
 annots = json.dumps(top_annots)
-open("data/annotations/ancestrydna.json", "w").write(annots)
+open("data/analysis/ancestrydna.json", "w").write(annots)
 
 top_annots = {}
 top_annots["keys"] = [
@@ -194,11 +298,35 @@ top_annots["keys"] = [
 ]
 top_annots["annots"] = clin_annots
 annots = json.dumps(top_annots)
-open("data/annotations/ancestrydna_tracks.json", "w").write(annots)
+open("data/analysis/ancestrydna_tracks.json", "w").write(annots)
 
-print("Number of skipped clinical variants:")
-print(num_skipped_clinvars)
+
+output.append("Number variants in AncestryDNA sample:")
+output.append(str(num_ancestrydna_rsids) + "\n")
+
+output.append("Number of variants in ClinVar analyzed:")
+output.append(str(len(rsids)) + "\n")
+
+output.append("Number of skipped clinical variants:")
+output.append(str(num_skipped_clinvars) + "\n")
 
 #for rs in clinical_alleles:
-#    print(rs)
-print("Pathogenic or likely pathogenic alleles: " + str(len(clinical_alleles)))
+#    output.append(rs)
+
+
+s = rs_summaries
+
+output.append(
+    "\nClinically significant variants in AncestryDNA sample:\n" +
+        "\tPathogenic: " + str(len(s["pathogenic"])) + "\n"
+        "\tLikely pathogenic: " + str(len(s["likely_pathogenic"])) + "\n"
+        "\tDrug response: " + str(len(s["drug_response"])) + "\n"
+)
+
+for key in rs_summaries:
+    for summary in rs_summaries[key]:
+        output.append(summary)
+
+output = "\n".join(output)
+
+open(output_file, "w").write(output)
