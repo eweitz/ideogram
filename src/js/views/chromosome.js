a7309e5eabe61b4be524301c41faf2ac3761da6e
zeleniy <zeleniy.spb@gmail.com>
2016-10-03 15:09:16 +0800

    Update chromosomes rendering approach. Fix bugs

--- src/js/views/chromosome.js
+++ src/js/views/chromosome.js
@@ -12,9 +12,7 @@ function Chromosome(model, config, ideo) {
     this._config = config;
     this._ideo = ideo;
     this._color = new Color(this._config);
-    this._currentArm = undefined;
-    this._width = this._config.chrWidth;
-    this._bump = this._width / 3.5;
+    this._bumpCoefficient = 5;
 }
 
 
@@ -34,7 +32,7 @@ Chromosome.getInstance = function(model, config, ideo) {
     } else {
         return new MetacentricChromosome(model, config, ideo);
     }
-}
+};
 
 
 /**
@@ -43,11 +41,102 @@ Chromosome.getInstance = function(model, config, ideo) {
  * @param {Selection} container
  * @param {Integer} chrSetNumber
  * @param {Integer} chrNumber
+ * @return {String[]}
  */
 Chromosome.prototype.render = function(container, chrSetNumber, chrNumber) {
+    /*
+     * Append bands container and apply clip-path on it.
+     */
+    container = container.append('g')
+        .attr('class', 'bands')
+        .attr("clip-path", "url(#" + this._model.id + "-chromosome-set-clippath)")
+    /*
+     * Render chromosome arms.
+     */
+    this._renderPArm(container, chrSetNumber, chrNumber);
+    this._renderQArm(container, chrSetNumber, chrNumber);
+    /*
+     * Push p arm shape string path.
+     */
+    var clipPath = [];
+    clipPath = clipPath.concat(this._getPArmShape());
+    clipPath = clipPath.concat(this._getQArmShape());
+    /*
+     * Render shapes.
+     */
+    var self = this;
+    container.append('g')
+        .attr('class', 'chromosome-border')
+        .selectAll('path')
+        .data(clipPath)
+        .enter()
+        .append('path')
+        .attr('fill', 'transparent')
+        .attr('stroke', function(d, i) {
+            return self._ideo._color.getBorderColor(chrSetNumber, chrNumber, i);
+        }).attr('stroke-width', 1)
+        .attr('d', function(d) {
+            return d.path;
+        }).attr('class', function(d) {
+            return d['class'];
+        });
 
-    var offset = this._renderPArm(container, chrSetNumber, chrNumber);
-    this._renderQArm(container, chrSetNumber, chrNumber, offset);
+    return clipPath;
+};
+
+
+/**
+ * Get chromosome's shape main values.
+ * @returns {Object}
+ */
+Chromosome.prototype._getShapeData = function() {
+    /*
+     * Properties description.
+     * x1 - left terminal start position
+     * x2 - centromere position
+     * x3 - right terminal end position
+     * w - chromosome width
+     * b - bump size
+     */
+    return {
+        x1 : 0,
+        x2 : this._model.bands.find(function(band) {
+                return band.name[0] == 'q';
+            }).px.start,
+        x3 : this._model.bands[this._model.bands.length - 1].px.stop,
+        w : this._config.chrWidth,
+        b : this._config.chrWidth / this._bumpCoefficient
+    };
+};
+
+
+Chromosome.prototype._getPArmShape = function() {
+
+    var d = this._getShapeData();
+
+    return {
+        'class' : '',
+        'path' : 'M' + d.b + ',0 ' +
+            'L' + (d.x2 - d.b) + ',0 ' +
+            'Q' + (d.x2 + d.b) + ',' + (d.w / 2) + ',' + (d.x2 - d.b) + ',' + d.w + ' ' +
+            'L' + d.b + ',' + d.w + ' ' +
+            'Q-' + d.b + ',' + (d.w / 2) + ',' + d.b + ',0'
+    };
+};
+
+
+Chromosome.prototype._getQArmShape = function() {
+
+    var d = this._getShapeData();
+
+    return {
+        'class' : '',
+        'path' : 'M' + (d.x2 + d.b) + ',0 ' +
+            'L' + (d.x3 - d.b) + ',0 ' +
+            'Q' + (d.x3 + d.b) + ',' + (d.w / 2) + ',' + (d.x3 - d.b) + ',' + d.w + ' ' +
+            'L' + (d.x2 + d.b) + ',' + d.w + ' ' +
+            'Q' + (d.x2 - d.b) + ',' + (d.w / 2) + ',' + (d.x2 + d.b) + ',0'
+    };
 };
 
 
@@ -60,16 +149,13 @@ Chromosome.prototype.render = function(container, chrSetNumber, chrNumber) {
  * @param {'p'|'q'} arm
  * @returns {Number}
  */
-Chromosome.prototype._renderBands = function(container, chrSetNumber, chrNumber, position, arm) {
+Chromosome.prototype._renderBands = function(container, chrSetNumber, chrNumber, bands, arm) {
 
     var self = this;
 
-    var x = position.x;
-
     container.selectAll("path.band." + arm)
-        .data(self._model.bands.filter(function(band) {
-            return band.name[0] == arm;
-        })).enter()
+        .data(bands)
+        .enter()
         .append("path")
         .attr("id", function(d, i) {
             return self._model.id + "-" + d.name.replace(".", "-");
@@ -77,7 +163,7 @@ Chromosome.prototype._renderBands = function(container, chrSetNumber, chrNumber,
             return 'band ' + arm + '-band ' + d.stain;
         }).attr("d", function(d, i) {
 
-            var start = self._ideo.round(d.px.start) + position.offset;
+            var start = self._ideo.round(d.px.start);// + position.offset;
             var length = self._ideo.round(d.px.width);
 
             x = start + length;
@@ -89,77 +175,38 @@ Chromosome.prototype._renderBands = function(container, chrSetNumber, chrNumber,
         }).style('fill', function(d) {
             return self._color.getArmColor(chrSetNumber, chrNumber, arm == 'p' ? 0 : 1);
         });
-
-    return {
-        offset: position.offset,
-        x: x
-    };
 };
 
 
 /**
  * Render chromosome's p arm.
- * @public
+ * @private
  * @param {Selection} container
  * @param {Integer} chrSetNumber
  * @param {Integer} chrNumber
- * @returns {Number}
  */
 Chromosome.prototype._renderPArm = function(container, chrSetNumber, chrNumber) {
 
-    var position = {
-        offset: 0,
-        x: 0
-    };
+    var bands = this._model.bands.filter(function(band) {
+        return band.name[0] == 'p';
+    });
 
-    position = this._renderLeftTerminal(container, chrSetNumber, chrNumber, position, 'p');
-    start = position.offset;
-
-    position = this._renderBands(container, chrSetNumber, chrNumber, position, 'p');
-    stop = position.x;
-
-    this._renderBorder(container, chrSetNumber, chrNumber, 'p', start, stop);
-
-    return this._renderRightTerminal(container, chrSetNumber, chrNumber, position, 'p');
+    this._renderBands(container, chrSetNumber, chrNumber, bands, 'p');
 };
 
 
 /**
  * Render chromosome's q arm.
+ * @private
  * @param {Selection} container
  * @param {Integer} chrSetNumber
  * @param {Integer} chrNumber
- * @param {Object} x
- * @returns {Number}
  */
-Chromosome.prototype._renderQArm = function(container, chrSetNumber, chrNumber, position) {
+Chromosome.prototype._renderQArm = function(container, chrSetNumber, chrNumber) {
 
-    var position = this._renderLeftTerminal(container, chrSetNumber, chrNumber, position, 'q');
-    start = position.x;
+    var bands = this._model.bands.filter(function(band) {
+        return band.name[0] == 'q';
+    });
 
-    position = this._renderBands(container, chrSetNumber, chrNumber, position, 'q');
-    stop = position.x;
-
-    this._renderBorder(container, chrSetNumber, chrNumber, 'q', start, stop);
-
-    return this._renderRightTerminal(container, chrSetNumber, chrNumber, position, 'q');
-};
-
-
-Chromosome.prototype._renderBorder = function(container, chrSetNumber, chrNumber, arm, from, to) {
-
-    container.append('line')
-        .attr('x1', from)
-        .attr('y1', 0)
-        .attr('x2', to)
-        .attr('y2', 0)
-        .style('stroke', this._color.getBorderColor(chrSetNumber, chrNumber, arm == 'p' ? 0 : 1))
-        .style('stroke-width', 0.5);
-    container.append('line')
-        .attr('x1', from)
-        .attr('y1', this._config.chrWidth)
-        .attr('x2', to)
-        .attr('y2', this._config.chrWidth)
-        .style('stroke', this._color.getBorderColor(chrSetNumber, chrNumber, arm == 'p' ? 0 : 1))
-        .style('stroke-width', 0.5);
+    this._renderBands(container, chrSetNumber, chrNumber, bands, 'q');
 };
--- src/js/views/telocentricChromosome.js
+++ src/js/views/telocentricChromosome.js
@@ -10,106 +10,49 @@ function TelocentricChromosome(model, config, ideo) {
 
     Chromosome.call(this, model, config, ideo);
     this._class = 'TelocentricChromosome';
+    this._pArmOffset = 4;
 }
 
 
 TelocentricChromosome.prototype = Object.create(Chromosome.prototype);
 
 
-TelocentricChromosome.prototype.getBumpsLength = function() {
+/**
+ * @override
+ */
+TelocentricChromosome.prototype._getPArmShape = function() {
 
-    return this._bump * 3 + 4;
+    var d = this._getShapeData();
+    d.o = this._pArmOffset;
+
+    return [{
+        'class' : 'acen',
+        'path' : 'M' + d.x2 + ',1' +
+            'L' + (d.x2 - d.o + 1) + ',1 ' + 
+            'L' + (d.x2 - d.o + 1) + ',' + (d.w - 1) + ' ' +
+            'L' + d.x2 + ',' + (d.w - 1)
+    }, {
+        'class' : 'gpos100',
+        'path' : 'M' + (d.x2 - d.o + 1) + ',0' +
+        'L' + (d.x2 - d.o) + ',0 ' + 
+        'L' + (d.x2 - d.o) + ',' + d.w + ' ' +
+        'L' + (d.x2 - d.o + 1) + ',' + d.w
+    }];
 };
 
 
 /**
- * Render chromosome right terminal.
- * @protected
- * @param {Selection} container
- * @param {Integer} chrSetNumber
- * @param {Integer} chrNumber
- * @param {Object} position
- * @param {'p'|'q'} arm
- * @returns {Number}
+ * @override
  */
-TelocentricChromosome.prototype._renderRightTerminal = function(container, chrSetNumber, chrNumber, position, arm) {
+TelocentricChromosome.prototype._getQArmShape = function() {
 
-    if (arm == 'p') {
-        return this._renderTelocentricRightTerminal(container, chrSetNumber, chrNumber, position, arm);
-    } else {
-        return this._renderMetacentricRightTerminal(container, chrSetNumber, chrNumber, position, arm);
-    }
-};
-
-
-/**
- * Render chromosome right telocentric terminal.
- * @private
- * @param {Selection} container
- * @param {Integer} chrSetNumber
- * @param {Integer} chrNumber
- * @param {Object} position
- * @param {'p'|'q'} arm
- * @returns {Number}
- */
-TelocentricChromosome.prototype._renderTelocentricRightTerminal = function(container, chrSetNumber, chrNumber, position, arm) {
-
-    var width = this._config.chrWidth;
-    var x = 4;
-
-    container.append('path')
-        .attr('class', 'band acen')
-        .attr('d', 'M1,1 L' + x + ',1 L' + x + ',' + (width - 1) + ' L1,' + (width - 1) + ' Z')
-        .style('stroke', 'black')
-        .style('stroke-width', 0.5);
-
-    container.append('line')
-        .attr('x1', 0)
-        .attr('y1', 0)
-        .attr('x2', 0)
-        .attr('y2', width)
-        .style('stroke', 'black')
-        .style('stroke-width', 2);
+    var d = this._getShapeData();
 
     return {
-        offset: position.offset + x - 2,
-        x: position.x + x
+        'class' : '',
+        'path' : 'M' + d.x2 + ',0 ' + 
+            'L' + (d.x3 - d.b) + ',0 ' +
+            'Q' + (d.x3 + d.b) + ',' + (d.w / 2) + ',' + (d.x3 - d.b) + ',' + d.w + ' ' +
+            'L' + d.x2 + ',' + d.w
     };
-};
-
-
-/**
- * Render chromosome right metacentric terminal.
- * @param container
- * @param chrSetNumber
- * @param chrNumber
- * @param x
- * @param arm
- * @returns
- */
-TelocentricChromosome.prototype._renderMetacentricRightTerminal = function(container, chrSetNumber, chrNumber, x, arm) {
-
-    var chromosome = new MetacentricChromosome(this._model, this._config, this._ideo);
-    chromosome._renderRightTerminal(container, chrSetNumber, chrNumber, x, arm);
-
-    return x;
-};
-
-
-/**
- * Render chromosome left terminal.
- * @private
- * @param {Selection} container
- * @param {Integer} chrSetNumber
- * @param {Integer} chrNumber
- * @param {Object} position
- * @param {'p'|'q'} arm
- * @returns {Number}
- */
-TelocentricChromosome.prototype._renderLeftTerminal = function(container, chrSetNumber, chrNumber, position, arm) {
-
-    return {
-        offset: position.offset,
-        x: position.x
-    }
 };
