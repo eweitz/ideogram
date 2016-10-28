9eee2f35a98756fd0b498a298d8a465b23379371
zeleniy <zeleniy.spb@gmail.com>
2016-10-28 16:53:02 +0300

    Create distributive build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var mochaPhantomJS = require('gulp-mocha-phantomjs');
 var mocha = require('gulp-mocha');
+var minify = require('gulp-minify');
+var concat = require('gulp-concat');
+var flatten = require('gulp-flatten');
+var ignore = require('gulp-ignore');
+var order = require('gulp-order');
 
 gulp.task('default', function () {
 
@@ -12,4 +17,35 @@ gulp.task('default', function () {
     //// gulp-mocha needs filepaths so you can't have any plugins before it
     // .pipe(mocha({reporter: 'spec'}));
 
+});
+
+gulp.task('dist', function() {
+  gulp.src('src/js/**/*.js')
+    .pipe(flatten())
+    .pipe(ignore.exclude(['ideogram.min.js']))
+    .pipe(order([
+        'modelAdapter.js',
+        'modelNoBandsAdapter.js',
+        'layout.js',
+        'horizontalLayout.js',
+        'verticalLayout.js',
+        'pairedLayout.js',
+        'smallLayout.js',
+        'ploidy.js',
+        'color.js',
+        'ploidyDescription.js',
+        'chromosome.js',
+        'telocentricChromosome.js',
+        'metacentricChromosome.js',
+        'ideogram.js',
+        'ideogram.filter.js'
+    ]))
+    .pipe(concat('ideogram.js'))
+    .pipe(minify({
+        ext:{
+            src:'.js',
+            min:'.min.js'
+        }
+    }))
+    .pipe(gulp.dest('dist'))
 });
