efbaf83135e884d1cefaea3f50817c3996abbce5
zeleniy <zeleniy.spb@gmail.com>
2016-11-04 22:55:52 +0300

    Fix annotations positions

--- dist/ideogram.js
+++ dist/ideogram.js
@@ -2810,8 +2810,10 @@ Ideogram.prototype.convertBpToPx = function(chr, bp) {
 
       bpToIscnScale = (band.iscn.stop - band.iscn.start)/(band.bp.stop - band.bp.start);
       iscn = band.iscn.start + (bp - band.bp.start) * bpToIscnScale;
-
-      px = band.px.start + (band.px.width * (iscn - band.iscn.start)/(band.iscn.stop - band.iscn.start));
+      /*
+       * TODO: What is 30 magic number??
+       */
+      px = 30 + band.px.start + (band.px.width * (iscn - band.iscn.start)/(band.iscn.stop - band.iscn.start));
 
       return px;
     }
