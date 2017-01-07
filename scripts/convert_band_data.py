22b472ced9175136d4bad5d9b5ec59e6b58b261a
Eric Weitz <eric.m.weitz@gmail.com>
2017-01-07 14:57:59 -0500

    Consolidate PHP band script in 'ploidy' to existing Python script; simplify TSV

--- scripts/convert_band_data.py
+++ scripts/convert_band_data.py
@@ -34,10 +34,41 @@ for input_file in f:
 
     output_file = out_dir + input_file.replace("tsv", "js")
 
-    rows = open(in_dir + input_file, "r").readlines()[1:]
-    for row in rows:
-    	columns = row.replace("\n", "").replace("\t", " ")
-    	output.append(columns)
+    rows = open(in_dir + input_file, "r").readlines()
+
+    if len(rows[0].split("\t")) == 4:
+        # e.g. ../data/bands/ncbi/ideogram_banana_v0.1.tsv
+        #chromosome	arm	bp_start	bp_stop
+        max_chr_length = 0
+        for row in rows[1:]:
+            columns = row.split("\t")
+            bp_stop = int(columns[3])
+            if bp_stop > max_chr_length:
+                max_chr_length = bp_stop
+        for row in rows[1:]:
+            columns = row.split("\t")
+
+            chr = columns[0]
+            arm = columns[1]
+            band = '1'
+            bp_start = int(columns[2])
+            bp_stop = int(columns[3])
+            iscn_start = '0'
+            iscn_stop = str(round(bp_stop - bp_start) / max_chr_length * 10000)
+
+            columns = [
+                chr, arm, band,
+                iscn_start, iscn_stop,
+                str(bp_start), str(bp_stop)
+            ]
+            output.append(" ".join(columns))
+
+    else:
+        # e.g. ../data/bands/ncbi/ideogram_9606_GCF_000001305.13_550_V1
+        # #chromosome	arm	band	iscn_start	iscn_stop	bp_start	bp_stop	stain	density
+        for row in rows[1:]:
+            columns = row.replace("\n", "").replace("\t", " ")
+            output.append(columns)
 
     output = json.dumps(output)
     output = "chrBands = " + output + ";"
