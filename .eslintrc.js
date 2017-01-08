89c87af52694ba94adf4743cf6cc16e9b6f6aa1d
Eric Weitz <eric.m.weitz@gmail.com>
2017-01-08 14:13:20 -0500

    Ignore some Google JavaScript style defaults

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "google"
+    "extends": "google",
+    "rules": {
+      // Override some default settings
+      "no-undef": "off",
+      "require-jsdoc": "off",
+      "valid-jsdoc": "off"
+    }
 };
