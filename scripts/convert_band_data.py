1a3d0477ed0e003d94e84ebfdde53fbc54239684
Eric Weitz <eric.m.weitz@gmail.com>
2017-01-18 23:58:05 -0500

    Execute code automatically when run as a script, but not when imported

--- scripts/convert_band_data.py
+++ scripts/convert_band_data.py
@@ -33,6 +33,16 @@ for (dirpath, dirnames, filenames) in walk(in_dir):
     f.extend(filenames)
     break
 
+# Those files had the following naming pattern:
+# * ideogram_$taxonomyID_$primaryAssemblyUnitAccession_$bandResolution_$version
+#
+# Transformed data files have been renamed to use the following pattern:
+# * $scientificName-$assemblyAccession-$bandResolution.js
+#
+# Default assembly accessions and band resolutions have been omitted.  The
+# renaming improves human intelligibility, and allows Ideogram.js to bypass API
+# calls to look up Taxonomy IDs and primary assembly unit acc.ver.  The latter
+# significantly improves performance for the most common use cases.
 output_mappings = {
     'ideogram_9606_GCF_000001305.14_850_V1': 'homo-sapiens',
     'ideogram_9606_GCF_000001305.14_550_V1': 'homo-sapiens-550',
@@ -43,57 +53,60 @@ output_mappings = {
     'ideogram_10116_GCF_000000225.4_NA_V1': 'rattus-norvegicus'
 }
 
-for input_file in f:
+def convert_band_data():
+    for input_file in f:
 
-    if input_file[-3:] != 'tsv':
-        # Skip e.g. README.md
-        continue
+        if input_file[-3:] != 'tsv':
+            # Skip e.g. README.md
+            continue
 
-    output = []
+        output = []
 
-    fn = input_file.split('.tsv')[0]
-    if fn in output_mappings:
-        fn = output_mappings[fn]
+        fn = input_file.split('.tsv')[0]
+        if fn in output_mappings:
+            fn = output_mappings[fn]
 
-    output_file = out_dir + fn + '.js'
+        output_file = out_dir + fn + '.js'
 
-    rows = open(in_dir + input_file, "r").readlines()
+        rows = open(in_dir + input_file, "r").readlines()
 
-    if len(rows[0].split("\t")) == 4:
-        # e.g. ../data/bands/ncbi/ideogram_banana_v0.1.tsv
-        #chromosome	arm	bp_start	bp_stop
-        max_chr_length = 0
-        for row in rows[1:]:
-            columns = row.split("\t")
-            bp_stop = int(columns[3])
-            if bp_stop > max_chr_length:
-                max_chr_length = bp_stop
-        for row in rows[1:]:
-            columns = row.split("\t")
+        if len(rows[0].split("\t")) == 4:
+            # e.g. ../data/bands/ncbi/ideogram_banana_v0.1.tsv
+            #chromosome	arm	bp_start	bp_stop
+            max_chr_length = 0
+            for row in rows[1:]:
+                columns = row.split("\t")
+                bp_stop = int(columns[3])
+                if bp_stop > max_chr_length:
+                    max_chr_length = bp_stop
+            for row in rows[1:]:
+                columns = row.split("\t")
 
-            chr = columns[0]
-            arm = columns[1]
-            band = '1'
-            bp_start = int(columns[2])
-            bp_stop = int(columns[3])
-            iscn_start = '0'
-            iscn_stop = str(round(bp_stop - bp_start) / max_chr_length * 10000)
+                chr = columns[0]
+                arm = columns[1]
+                band = '1'
+                bp_start = int(columns[2])
+                bp_stop = int(columns[3])
+                iscn_start = '0'
+                iscn_stop = str(round(bp_stop - bp_start) / max_chr_length * 10000)
 
-            columns = [
-                chr, arm, band,
-                iscn_start, iscn_stop,
-                str(bp_start), str(bp_stop)
-            ]
-            output.append(" ".join(columns))
+                columns = [
+                    chr, arm, band,
+                    iscn_start, iscn_stop,
+                    str(bp_start), str(bp_stop)
+                ]
+                output.append(" ".join(columns))
 
-    else:
-        # e.g. ../data/bands/ncbi/ideogram_9606_GCF_000001305.13_550_V1
-        # #chromosome	arm	band	iscn_start	iscn_stop	bp_start	bp_stop	stain	density
-        for row in rows[1:]:
-            columns = row.replace("\n", "").replace("\t", " ")
-            output.append(columns)
+        else:
+            # e.g. ../data/bands/ncbi/ideogram_9606_GCF_000001305.13_550_V1
+            # #chromosome	arm	band	iscn_start	iscn_stop	bp_start	bp_stop	stain	density
+            for row in rows[1:]:
+                columns = row.replace("\n", "").replace("\t", " ")
+                output.append(columns)
 
-    output = json.dumps(output)
-    output = "chrBands = " + output + ";"
+        output = json.dumps(output)
+        output = "chrBands = " + output + ";"
+        open(output_file, "w").write(output)
 
-    open(output_file, "w").write(output)
+if __name__ == '__main__':
+    convert_band_data()
