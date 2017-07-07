33bc3d0adee437615742182b8906a2a7666e8d65
Eric Weitz <eric.m.weitz@gmail.com>
2017-07-06 23:26:23 -0400

    Refine dependencies

--- src/js/core.js
+++ src/js/core.js
@@ -235,6 +235,18 @@ export class Ideogram {
   }
 
   /**
+  * e.g. "Homo sapiens" -> "homo-sapiens"
+  */
+  static slugify(value) {
+    return value.toLowerCase().replace(' ', '-')
+  }
+
+  static naturalSort(a,b) {
+    // https://github.com/overset/javascript-natural-sort
+    var q,r,c=/(^([+\-]?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?(?=\D|\s|$))|^0x[\da-fA-F]+$|\d+)/g,d=/^\s+|\s+$/g,e=/\s+/g,f=/(^([\w ]+,?[\w ]+)?[\w ]+,?[\w ]+\d+:\d+(:\d+)?[\w ]?|^\d{1,4}[\/\-]\d{1,4}[\/\-]\d{1,4}|^\w+, \w+ \d+, \d{4})/,g=/^0x[0-9a-f]+$/i,h=/^0/,i=function(a){return(Ideogram.naturalSort.insensitive&&(""+a).toLowerCase()||""+a).replace(d,"")},j=i(a),k=i(b),l=j.replace(c,"\0$1\0").replace(/\0$/,"").replace(/^\0/,"").split("\0"),m=k.replace(c,"\0$1\0").replace(/\0$/,"").replace(/^\0/,"").split("\0"),n=parseInt(j.match(g),16)||1!==l.length&&Date.parse(j),o=parseInt(k.match(g),16)||n&&k.match(f)&&Date.parse(k)||null,p=function(a,b){return(!a.match(h)||1==b)&&parseFloat(a)||a.replace(e," ").replace(d,"")||0};if(o){if(n<o)return-1;if(n>o)return 1}for(var s=0,t=l.length,u=m.length,v=Math.max(t,u);s<v;s++){if(q=p(l[s]||"",t),r=p(m[s]||"",u),isNaN(q)!==isNaN(r))return isNaN(q)?1:-1;if(/[^\x00-\x80]/.test(q+r)&&q.localeCompare){var w=q.localeCompare(r);return w/Math.abs(w)}if(q<r)return-1;if(q>r)return 1}
+  }
+
+  /**
   * Gets chromosome band data from a
   * TSV file, or, if band data is prefetched, from an array
   *
@@ -1885,7 +1897,7 @@ export class Ideogram {
     // bIsPlastid = bIsMT && b.name !== 'MT';
 
     if (aIsNuclear && bIsNuclear) {
-      return naturalSort(a.name, b.name);
+      return Ideogram.naturalSort(a.name, b.name);
     } else if (!aIsNuclear && bIsNuclear) {
       return 1;
     } else if (aIsMT && bIsCP) {
@@ -2261,7 +2273,7 @@ export class Ideogram {
         bandsByChr = ideo.getBands(bandData, taxid, chrs);
 
         chrs = Object.keys(bandsByChr).sort(function(a, b) {
-          return naturalSort(a, b);
+          return Ideogram.naturalSort(a, b);
         });
 
         ideo.config.chromosomes[taxid] = chrs.slice();
@@ -2353,7 +2365,9 @@ export class Ideogram {
         accession = assemblies[ideo.config.assembly];
 
         bandFileName = [];
-        bandFileName.push(slugify(ideo.organisms[taxid].scientificName));
+        bandFileName.push(
+          Ideogram.slugify(ideo.organisms[taxid].scientificName)
+        );
         if (accession !== assemblies.default) {
           bandFileName.push(accession);
         }
