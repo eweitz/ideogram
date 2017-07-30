980bbf01cbccccbde16afad308784a59bb1be04f
Eric Weitz <eric.m.weitz@gmail.com>
2017-07-30 19:31:19 -0400

    Generalize chromosome list, add watch task

--- src/js/parsers/bed-parser.js
+++ src/js/parsers/bed-parser.js
@@ -31,12 +31,7 @@ export class BedParser {
 
     annots = [];
 
-    // Horrible.  Remove hard-coding.
-    chrs = [
-      "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-      "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-      "21", "22", "X", "Y"
-    ];
+    chrs = Object.keys(ideo.chromosomes[ideo.config.taxid]);
 
     for (i = 0; i < chrs.length; i++) {
       chr = chrs[i];
