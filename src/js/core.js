3cbb42e8bf2246a0595c83adb27c0fe6065b96f2
Eric Weitz <eric.m.weitz@gmail.com>
2017-08-28 21:14:18 -0400

    Condense inline style rules

--- src/js/core.js
+++ src/js/core.js
@@ -1790,6 +1790,67 @@ export default class Ideogram {
       '</linearGradient>';
     }
 
+    css = '<style>' +
+      'svg#_ideogram  {padding-left: 5px;} ' +
+      'svg#_ideogram .labeled {padding-left: 15px;} ' +
+      'svg#_ideogram.labeledLeft {padding-left: 15px; padding-top: 15px;} ' +
+      // Tahoma has great readability and space utilization at small sizes
+      // More: http://ux.stackexchange.com/a/3334
+      '#_ideogram text {font: 9px Tahoma; fill: #000;} ' +
+      '#_ideogram .italic {font-style: italic;} ' +
+      // Fill below is fallback for IE11
+      '#_ideogram .chromosome {cursor: pointer; fill: #AAA;}' +
+      '#_ideogram .chrSetLabel {font-weight: bolder;}' +
+      '#_ideogram .ghost {opacity: 0.2;}' +
+      '#_ideogram .hidden {display: none;}' +
+      '#_ideogram .bandLabelStalk line {stroke: #AAA; stroke-width: 1;}' +
+      '#_ideogram .syntenyBorder {stroke:#AAA;stroke-width:1;}' +
+      '#_ideogram .brush .selection {' +
+      '  fill: #F00;' +
+      '  stroke: #F00;' +
+      '  fill-opacity: .3;' +
+      '  shape-rendering: crispEdges;' +
+      '}' +
+      '#_ideogram .noBands {fill: #AAA;}' +
+      // NCBI stain density colors
+      '#_ideogram .gneg {fill: #FFF}' +
+      '#_ideogram .gpos25 {fill: #BBB}' +
+      '#_ideogram .gpos33 {fill: #AAA}' +
+      '#_ideogram .gpos50 {fill: #888}' +
+      '#_ideogram .gpos66 {fill: #666}' +
+      '#_ideogram .gpos75 {fill: #444}' +
+      '#_ideogram .gpos100 {fill: #000}' +
+      '#_ideogram .gpos {fill: #000}' +
+      '#_ideogram .acen {fill: #FDD}' +
+      '#_ideogram .stalk {fill: #CCE;}' +
+      '#_ideogram .gvar {fill: #DDF}' +
+      // Used when overlaid with annotations
+      '#_ideogram.faint .gneg {fill: #FFF}' +
+      '#_ideogram.faint .gpos25 {fill: #EEE}' +
+      '#_ideogram.faint .gpos33 {fill: #EEE}' +
+      '#_ideogram.faint .gpos50 {fill: #EEE}' +
+      '#_ideogram.faint .gpos66 {fill: #EEE}' +
+      '#_ideogram.faint .gpos75 {fill: #EEE}' +
+      '#_ideogram.faint .gpos100 {fill: #DDD}' +
+      '#_ideogram.faint .gpos {fill: #DDD}' +
+      '#_ideogram.faint .acen {fill: #FEE}' +
+      '#_ideogram.faint .stalk {fill: #EEF;}' +
+      '#_ideogram.faint .gvar {fill: #EEF}' +
+      '.gneg {fill: url("#gneg")} ' +
+      '.gpos25 {fill: url("#gpos25")} ' +
+      '.gpos33 {fill: url("#gpos33")} ' +
+      '.gpos50 {fill: url("#gpos50")} ' +
+      '.gpos66 {fill: url("#gpos66")} ' +
+      '.gpos75 {fill: url("#gpos75")} ' +
+      '.gpos100 {fill: url("#gpos100")} ' +
+      '.gpos {fill: url("#gpos100")} ' +
+      '.acen {fill: url("#acen")} ' +
+      '.stalk {fill: url("#stalk")} ' +
+      '.gvar {fill: url("#gvar")} ' +
+      '.noBands {fill: url("#noBands")} ' +
+      '.chromosome {fill: url("#noBands")} ' +
+      '</style>';
+
     gradients +=
     '<pattern id="stalk" width="2" height="1" patternUnits="userSpaceOnUse" ' +
       'patternTransform="rotate(30 0 0)">' +
@@ -1803,124 +1864,8 @@ export default class Ideogram {
        '<line x1="0" y1="0" x2="0" y2="100%" style="stroke:#99C; ' +
           'stroke-width:0.7;" />' +
     '</pattern>';
-
     gradients = "<defs>" + gradients + "</defs>";
-    css = "<style>" +
-    '      body {' +
-    '      font: 12px Arial;' +
-    '      line-height: 19.6px;' +
-    '    }' +
-    '' +
-    '    svg#_ideogram  {' +
-    '      padding-left: 5px;' +
-    '    }' +
-    '' +
-    '    svg#_ideogram .labeled {' +
-    '      padding-left: 15px;' +
-    '    }' +
-    '' +
-    '    svg#_ideogram.labeledLeft {' +
-    '      padding-left: 15px;' +
-    '      padding-top: 15px;' +
-    '    }' +
-    '' +
-    '#_ideogram text {' +
-    '      /*' +
-    '      Tahoma has excellent readability and' +
-    '      space utilization at small sizes.' +
-    '      More: http://ux.stackexchange.com/a/3334' +
-    '      */' +
-    '      font: 9px Tahoma;' +
-    '      fill: #000;' +
-    '    }' +
-    '' +
-    '#_ideogram .italic {' +
-    '      font-style: italic;' +
-    '    }' +
-    '' +
-    '#_ideogram .chromosome {' +
-    '      cursor: pointer;' +
-    '      fill: #AAA; /* Fallback for IE11 */' +
-    '    }' +
-    '' +
-    '#_ideogram .chrSetLabel {' +
-    '      font-weight: bolder;' +
-    '    }' +
-    '' +
-    '#_ideogram .ghost {' +
-    '      opacity: 0.2;' +
-    '    }' +
-    '' +
-    '#_ideogram .hidden {' +
-    '      display: none;' +
-    '    }' +
-    '' +
-    '#_ideogram path.band {' +
-    '      /* opacity: 0.5; */' +
-    '    }' +
-    '' +
-    '#_ideogram .bandLabelStalk line {' +
-    '      stroke: #AAA;' +
-    '      stroke-width: 1;' +
-    '    }' +
-    '' +
-    '#_ideogram .syntenyBorder {' +
-    '      stroke:#AAA;' +
-    '      stroke-width:1;' +
-    '    }' +
-    '' +
-    '#_ideogram .brush .selection {' +
-    '      fill: #F00;' +
-    '      stroke: #F00;' +
-    '      fill-opacity: .3;' +
-    '      shape-rendering: crispEdges;' +
-    '    }' +
-    '' +
-    '#_ideogram .noBands {' +
-    '  fill: #AAA;' +
-    '}' +
-    '' +
-    '/* NCBI stain density colors */' +
-    '#_ideogram .gneg {fill: #FFF}' +
-    '#_ideogram .gpos25 {fill: #BBB}' +
-    '#_ideogram .gpos33 {fill: #AAA}' +
-    '#_ideogram .gpos50 {fill: #888}' +
-    '#_ideogram .gpos66 {fill: #666}' +
-    '#_ideogram .gpos75 {fill: #444}' +
-    '#_ideogram .gpos100 {fill: #000}' +
-    '#_ideogram .gpos {fill: #000}' +
-    '#_ideogram .acen {fill: #FDD}' +
-    '#_ideogram .stalk {fill: #CCE;}' +
-    '#_ideogram .gvar {fill: #DDF}' +
-    '    /* Used when overlaid with annotations */' +
-    '#_ideogram.faint .gneg {fill: #FFF}' +
-    '#_ideogram.faint .gpos25 {fill: #EEE}' +
-    '#_ideogram.faint .gpos33 {fill: #EEE}' +
-    '#_ideogram.faint .gpos50 {fill: #EEE}' +
-    '#_ideogram.faint .gpos66 {fill: #EEE}' +
-    '#_ideogram.faint .gpos75 {fill: #EEE}' +
-    '#_ideogram.faint .gpos100 {fill: #DDD}' +
-    '#_ideogram.faint .gpos {fill: #DDD}' +
-    '#_ideogram.faint .acen {fill: #FEE}' +
-    '#_ideogram.faint .stalk {fill: #EEF;}' +
-    '#_ideogram.faint .gvar {fill: #EEF}' +
-    '.gneg {fill: url("#gneg")} ' +
-    '.gpos25 {fill: url("#gpos25")} ' +
-    '.gpos33 {fill: url("#gpos33")} ' +
-    '.gpos50 {fill: url("#gpos50")} ' +
-    '.gpos66 {fill: url("#gpos66")} ' +
-    '.gpos75 {fill: url("#gpos75")} ' +
-    '.gpos100 {fill: url("#gpos100")} ' +
-    '.gpos {fill: url("#gpos100")} ' +
-    '.acen {fill: url("#acen")} ' +
-    '.stalk {fill: url("#stalk")} ' +
-    '.gvar {fill: url("#gvar")} ' +
-    '.noBands {fill: url("#noBands")} ' +
-    '.chromosome {fill: url("#noBands")} ' +
-  '</style>';
     gradients = css + gradients;
-
-    // alert(gradients)
 
     return gradients;
   }
