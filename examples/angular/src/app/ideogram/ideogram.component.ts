e4594a45a8609f8a20aed18dc6fd79035e8ec415
Eric Weitz <eric.m.weitz@gmail.com>
2017-09-25 20:11:43 -0400

    Add Angular component scaffolding for Ideogram

---
+++ examples/angular/src/app/ideogram/ideogram.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ideogram',
+  templateUrl: './ideogram.component.html',
+  styleUrls: ['./ideogram.component.css']
+})
+export class IdeogramComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
