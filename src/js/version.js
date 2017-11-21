613ac72d8a351518646a0984d9618478a507f911
Eric Weitz <eric.m.weitz@gmail.com>
2017-11-21 08:58:53 -0500

    Update paths

--- src/js/version.js
+++ src/js/version.js
@@ -1,13 +1,2 @@
-/**
- * @fileoverview Reports version of Ideogram.js.  Updated by build script.
- *
- * This module is imported by ideogram.js, which uses it in a static variable
- * to report the current version of Ideogram.js.  The "version" value below
- * is updated by /scripts/prepublish.js as part of each build, to use the
- * value set in /package.json.
- *
- * Make any change to this "version" value in package.json, not here.
- */
-
 var version = '0.11.0';
 export default version;
