a21c9e1e09e65e341618811bc65a06df2109a639
Eric Weitz <eric.m.weitz@gmail.com>
2017-12-28 17:06:07 -0500

    Add Ideogram to React example

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -2,18 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Ideogram from 'ideogram';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Ideogram.js in React!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AppIdeogram/>
       </div>
+    );
+  }
+}
+
+class AppIdeogram extends Component {
+
+  componentDidMount() {
+    return new Ideogram({
+      organism: 'human',
+      dataDir: 'https://unpkg.com/ideogram@0.13.0/dist/data/bands/native/',
+      container: '#ideo-container'
+    });
+  }
+
+  render() {
+    return (
+      <div id="ideo-container"></div>
     );
   }
 }
