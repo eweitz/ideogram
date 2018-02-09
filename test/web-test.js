202d8921b5fee04dbe5dd5562219293e3f24d9aa
Eric Weitz <eric.m.weitz@gmail.com>
2018-02-08 23:58:40 -0500

    Test fix for alignment of annotation bars, sliders, bands

--- test/web-test.js
+++ test/web-test.js
@@ -616,6 +616,94 @@ describe("Ideogram", function() {
     ideogram = new Ideogram(config);
   });
 
+  it("should have annotations and brushes aligned with base pairs", function(done) {
+    // Tests fix for https://github.com/eweitz/ideogram/issues/91
+    // and related issues.
+
+    function getLeft(selector) {
+      return  Math.round(document
+        .querySelector(selector)
+        .getBoundingClientRect().x);
+    }
+
+    function getRight(selector) {
+      return  Math.round(document
+        .querySelector(selector)
+        .getBoundingClientRect().right);
+    }
+
+    var config = {
+      organism: 'human',
+      assembly: 'GRCh37',
+      chrHeight: 800,
+      dataDir: '/dist/data/bands/native/',
+
+      annotationsLayout: 'histogram',
+      chromosomes: ['17'],
+
+      brush: true,
+      onBrushMove: function () {},
+      onLoad: function () {
+        this.createBrush(1, 2);
+        this.createBrush(40900000, 44900000);
+        this.createBrush(81094108, 81094109);
+
+        // Closest test for https://github.com/eweitz/ideogram/issues/91
+        var bandQ2131Left = getLeft('#chr17-9606-q21-31');
+        var bandQ2131AnnotLeft = getLeft('#chr17-9606 .annot:nth-child(191)');
+        var bandQ2131BrushLeft = getLeft('#_ideogram > g:nth-child(6) > rect.selection');
+        assert.equal(bandQ2131AnnotLeft, bandQ2131Left);
+        assert.equal(bandQ2131AnnotLeft, bandQ2131BrushLeft);
+
+        // Check alignment at far left
+        var firstBpAnnotLeft = getLeft('#chr17-9606 > .annot:nth-child(51)');
+        var firstBpSliderLeft = getLeft('#_ideogram > g:nth-child(5) > rect.selection');
+        var firstBpLeft = getLeft('#chr17-9606');
+        assert.equal(firstBpAnnotLeft, firstBpSliderLeft);
+        assert.equal(firstBpSliderLeft, firstBpLeft);
+
+        // Check alignment at far right
+        var lastBpAnnotRight = getRight('#chr17-9606 > .annot:nth-child(317)');
+        var lastBpSliderRight = getRight('#_ideogram > g:nth-child(7) > rect.selection');
+        var lastBpRight = getRight('#chr17-9606');
+        assert.isBelow(Math.abs(lastBpAnnotRight - lastBpSliderRight), 2);
+        assert.isBelow(Math.abs(lastBpSliderRight - lastBpRight), 2);
+
+        done();
+      },
+
+      orientation: 'horizontal',
+      showBandLabels: true, // only work in horizontal mode
+
+      annotations: [{
+        name: 'first_band',
+        chr: '17',
+        start: 1,
+        stop: 2
+      },
+        {
+          name: 'band_q21-31',
+          chr: '17',
+          start: 40900000,
+          stop: 40900001
+        },
+        {
+          name: 'last_band_start',
+          chr: '17',
+          start: 75300000,
+          stop: 75300001
+        },
+        {
+          name: 'last_band_stop',
+          chr: '17',
+          start: 81195208,
+          stop: 81195209
+        }]
+    };
+
+    var ideogram = new Ideogram(config);
+  });
+
   it("should have 12 chromosomes per row in small layout example", function(done) {
       // Tests use case from ../examples/layout_small.html
 
