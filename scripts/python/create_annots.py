bcf76e4adc10517f2d5f0802c187113b1f79d455
Eric Weitz <eric.m.weitz@gmail.com>
2018-06-19 13:26:02 -0400

    Improve comments, extract variables, use single quotes

--- scripts/python/create_annots.py
+++ scripts/python/create_annots.py
@@ -1,4 +1,4 @@
-''' Creates simulated genome annotation data
+""" Creates simulated genome annotation data
 
 Data is currently simulated single-nucleotide variations (SNVs).
 
@@ -10,7 +10,7 @@ TODO:
     - Clinical significance
     - Transcript accession
     - HGVS expression
-'''
+"""
 
 import json
 import random
@@ -30,7 +30,6 @@ parser.add_argument('--assembly',
                     help='Genome assembly reference to use: GRCh38 or GRCh37',
                     default='GRCh38')
 
-
 args = parser.parse_args()
 output_dir = args.output_dir
 num_annots = args.num_annots
@@ -39,31 +38,31 @@ assembly = args.assembly
 annots = []
 
 chrs = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-    "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-    "21", "22", "X", "Y"
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+    '21', '22', 'X', 'Y'
 ]
 
 lengths_GRCh38 = {
-    "1": 248956422, "2": 242193529, "3": 198295559,
-    "4": 190214555, "5": 181538259, "6": 170805979,
-    "7": 159345973, "8": 145138636, "9": 138394717,
-    "10": 133797422, "11": 135086622, "12": 133275309,
-    "13": 114364328, "14": 107043718, "15": 101991189,
-    "16": 90338345,    "17": 83257441, "18": 80373285,
-    "19": 58617616, "20": 64444167, "21": 46709983,
-    "22": 50818468, "X": 156040895, "Y": 57227415
+    '1': 248956422, '2': 242193529, '3': 198295559,
+    '4': 190214555, '5': 181538259, '6': 170805979,
+    '7': 159345973, '8': 145138636, '9': 138394717,
+    '10': 133797422, '11': 135086622, '12': 133275309,
+    '13': 114364328, '14': 107043718, '15': 101991189,
+    '16': 90338345, '17': 83257441, '18': 80373285,
+    '19': 58617616, '20': 64444167, '21': 46709983,
+    '22': 50818468, 'X': 156040895, 'Y': 57227415
 }
 
 lengths_GRCh37 = {
-    "1": 249250621, "2": 243199373, "3": 198022430,
-    "4": 191154276, "5": 180915260, "6": 171115067,
-    "7": 159138663, "8": 146364022, "9": 141213431,
-    "10": 135534747, "11": 135006516, "12": 133851895,
-    "13": 115169878, "14": 107349540, "15": 102531392,
-    "16": 90354753, "17": 81195210, "18": 78077248,
-    "19": 59128983, "20": 63025520, "21": 48129895,
-    "22": 51304566, "X": 155270560, "Y": 59373566
+    '1': 249250621, '2': 243199373, '3': 198022430,
+    '4': 191154276, '5': 180915260, '6': 171115067,
+    '7': 159138663, '8': 146364022, '9': 141213431,
+    '10': 135534747, '11': 135006516, '12': 133851895,
+    '13': 115169878, '14': 107349540, '15': 102531392,
+    '16': 90354753, '17': 81195210, '18': 78077248,
+    '19': 59128983, '20': 63025520, '21': 48129895,
+    '22': 51304566, 'X': 155270560, 'Y': 59373566
 }
 
 if assembly == 'GRCh38':
@@ -72,9 +71,12 @@ else:
     chr_lengths = lengths_GRCh37
 
 for chr in chrs:
-    annots.append({"chr": chr, "annots": []});
+    annots.append({'chr': chr, 'annots': []})
 
-#
+# Percentage of annotations in each track
+# For example:
+#   [0] * 5 + [1] * 80 + [2] * 15
+#   means 5% in 1st track, 80% in 2nd, 15% in 3rd
 track_index_pool = [0] * 5 + [1] * 80 + [2] * 15
 pool_size = len(track_index_pool)
 
@@ -86,26 +88,29 @@ while i < num_annots:
     start = int((i * chr_lengths[chrs[chr]])/1000 + 1)
     length = 0
 
+    random_index = random.randrange(0, pool_size - 1)
+    track_index = track_index_pool[random_index]
+
     annot = [
-        "rs" + j,
+        'rs' + j,
         start,
         length,
-        track_index_pool[random.randrange(0, pool_size - 1)]
+        track_index
     ]
 
-    annots[chr]["annots"].append(annot)
+    annots[chr]['annots'].append(annot)
 
     i += 1
 
 top_annots = {}
-top_annots["keys"] = ["name", "start", "length", "trackIndex"]
-top_annots["annots"] = annots
+top_annots['keys'] = ['name', 'start', 'length', 'trackIndex']
+top_annots['annots'] = annots
 annots = json.dumps(top_annots)
 
 num_annots = str(num_annots)
-output_path = output_dir + "/" + num_annots + "_virtual_snvs.json"
+output_path = output_dir + '/' + num_annots + '_virtual_snvs.json'
 
-open(output_path, "w").write(annots)
+open(output_path, 'w').write(annots)
 print(
     'Output ' + num_annots + ' annotations ' +
     'on assembly ' + assembly + ' ' +
