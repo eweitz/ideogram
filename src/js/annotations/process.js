7bb874370af24ff7c03e59a584577efe938a4a64
Eric Weitz <eric.m.weitz@gmail.com>
2018-08-27 19:39:11 -0400

    Digest processAnnotData into several smaller methods

--- src/js/annotations/process.js
+++ src/js/annotations/process.js
@@ -1,49 +1,174 @@
+// Default colors for tracks of annotations
+var colorMap = [
+  ['F00'], // If there is 1 track, then color it red.
+  ['F00', '88F'], // If 2 tracks, color one red and one light blue.
+  ['F00', 'CCC', '88F'], // If 3, color one red, one grey, one light blue.
+  ['F00', 'FA0', '0AF', '88F'], // And so on.
+  ['F00', 'FA0', 'CCC', '0AF', '88F'],
+  ['F00', 'FA0', '875', '578', '0AF', '88F'],
+  ['F00', 'FA0', '875', 'CCC', '578', '0AF', '88F'],
+  ['F00', 'FA0', '7A0', '875', '0A7', '578', '0AF', '88F'],
+  ['F00', 'FA0', '7A0', '875', 'CCC', '0A7', '578', '0AF', '88F'],
+  ['F00', 'FA0', '7A0', '875', '552', '255', '0A7', '578', '0AF', '88F']
+];
+
 /**
- * Proccesses genome annotation data.
- *
- * This method converts raw annotation data from server, which is structured as
- * an array of arrays, into a more verbose data structure consisting of an
- * array of objects.  It also adds pixel offset information.
+ * Ensure annotation containers are ordered by chromosome.
  */
-function processAnnotData(rawAnnots) {
-  var keys, numTracks, i, j, k, m, n, annot, annots, thisAnnot, annotsByChr,
-    chr, chrs, chrModel, ra, startPx, stopPx, px, annotTrack,
-    unorderedAnnots, colorMap, colors, omittedAnnots, numOmittedTracks, numAvailTracks,
-    ideo = this,
-    config = ideo.config;
+function orderAnnotContainers(annots, ideo) {
+  var unorderedAnnots, i, j, annot, chr, chrs;
 
-  omittedAnnots = {};
-
-  colorMap = [
-    ['F00'],
-    ['F00', '88F'],
-    ['F00', 'CCC', '88F'],
-    ['F00', 'FA0', '0AF', '88F'],
-    ['F00', 'FA0', 'CCC', '0AF', '88F'],
-    ['F00', 'FA0', '875', '578', '0AF', '88F'],
-    ['F00', 'FA0', '875', 'CCC', '578', '0AF', '88F'],
-    ['F00', 'FA0', '7A0', '875', '0A7', '578', '0AF', '88F'],
-    ['F00', 'FA0', '7A0', '875', 'CCC', '0A7', '578', '0AF', '88F'],
-    ['F00', 'FA0', '7A0', '875', '552', '255', '0A7', '578', '0AF', '88F']
-  ];
-
-  keys = rawAnnots.keys;
-  rawAnnots = rawAnnots.annots;
-
-  numTracks = config.numAnnotTracks;
-  numAvailTracks = ideo.numAvailTracks;
-
-  colors = colorMap[numAvailTracks - 1];
-
-  if (numTracks > 10) {
-    console.error(
-      'Ideogram only displays up to 10 tracks at a time.  ' +
-      'You specified ' + numTracks + ' tracks.  ' +
-      'Perhaps consider a different way to visualize your data.'
-    );
+  unorderedAnnots = annots;
+  annots = [];
+  chrs = ideo.chromosomesArray;
+  for (i = 0; i < chrs.length; i++) {
+    chr = chrs[i].name;
+    for (j = 0; j < unorderedAnnots.length; j++) {
+      annot = unorderedAnnots[j];
+      if (annot.chr === chr) {
+        annots.push(annot);
+      }
+    }
   }
 
+  return annots;
+}
+
+/**
+ * Add client annotations, as in annotations-tracks.html
+ */
+function addClientAnnot(annots, annot, config, ra, m) {
+  var annotTrack;
+
+  annot.trackIndex = ra[3];
+  annotTrack = config.annotationTracks[annot.trackIndex];
+  if (annotTrack.color) {
+    annot.color = annotTrack.color;
+  }
+  if (annotTrack.shape) {
+    annot.shape = annotTrack.shape;
+  }
+
+  annots[m].annots.push(annot);
+
+  return annots;
+}
+
+/**
+ * Add sparse server annotations, as in annotations-track-filters.html
+ */
+function addSparseServertAnnot(annot, ra, colors, omittedAnnots, annots, numTracks, m) {
+  annot.trackIndex = ra[3];
+  annot.trackIndexOriginal = ra[4];
+  annot.color = '#' + colors[annot.trackIndexOriginal];
+
+  // Catch annots that will be omitted from display
+  if (annot.trackIndex > numTracks - 1) {
+    if (annot.trackIndex in omittedAnnots) {
+      omittedAnnots[annot.trackIndex].push(annot);
+    } else {
+      omittedAnnots[annot.trackIndex] = [annot];
+    }
+    return [annots, omittedAnnots];
+  }
+  annots[m].annots.push(annot);
+
+  return [annots, omittedAnnots];
+}
+
+function addDenseServerAnnot(keys, annots, annot, m) {
+  var n, thisAnnot;
+
+  // Dense server annotations
+  for (n = 4; n < keys.length; n++) {
+    thisAnnot = Object.assign({}, annot); // copy by value
+    thisAnnot.trackIndex = n - 4;
+    thisAnnot.trackIndexOriginal = n - 3;
+    thisAnnot.color = '#' + colors[thisAnnot.trackIndexOriginal];
+    annots[m].annots.push(thisAnnot);
+  }
+
+  return annots;
+}
+
+/**
+ * Basic client annotations, as in annotations-basic.html
+ * and annotations-external.html
+ */
+function addBasicClientAnnot(annots, annot, config, m) {
+  annot.trackIndex = 0;
+  if (!annot.color) {
+    annot.color = config.annotationsColor;
+  }
+  if (!annot.shape) {
+    annot.shape = 'triangle';
+  }
+  annots[m].annots.push(annot);
+
+  return annots;
+}
+
+function addAnnot(annot, keys, ra, config, omittedAnnots, annots, m,
+  numAvailTracks, numTracks, colors) {
+
+  if (config.annotationTracks) {
+    annots = addClientAnnot(annots, annot, config, ra, m);
+  } else if (keys[3] === 'trackIndex' && numAvailTracks !== 1) {
+    [annots, omittedAnnots] = 
+      addSparseServertAnnot(annot, ra, colors, omittedAnnots, annots, numTracks, m);
+  } else if (
+    keys.length > 3 &&
+    keys[3] in {trackIndex: 1, color: 1, shape: 1} === false &&
+    keys[4] === 'trackIndexOriginal'
+  ) {
+    annots = addDenseServerAnnot(keys, annots, annot, m);
+  } else {
+    annots = addBasicClientAnnot(annots, annot, config, m);
+  }
+
+  return [annots, omittedAnnots];
+}
+
+function addAnnotsForChr(annots, omittedAnnots, annotsByChr, chr, chrModel,
+  colors, m, keys, numTracks, ideo, i, numAvailTracks) {
+  var j, k, annot, startPx, stopPx, px, ra,
+    config = ideo.config;
+
+  for (j = 0; j < annotsByChr.annots.length; j++) {
+    ra = annotsByChr.annots[j];
+    annot = {};
+
+    for (k = 0; k < keys.length; k++) {
+      annot[keys[k]] = ra[k];
+    }
+
+    annot.stop = annot.start + annot.length;
+
+    startPx = ideo.convertBpToPx(chrModel, annot.start);
+    stopPx = ideo.convertBpToPx(chrModel, annot.stop);
+    px = Math.round((startPx + stopPx) / 2);
+
+    annot.chr = chr;
+    annot.chrIndex = i;
+    annot.px = px;
+    annot.startPx = startPx;
+    annot.stopPx = stopPx;  
+
+    [annots, omittedAnnots] = 
+      addAnnot(annot, keys, ra, config, omittedAnnots, annots, m, numAvailTracks, numTracks, colors);
+  }
+
+  return [annots, omittedAnnots];
+}
+
+function addAnnots(rawAnnots, keys, numTracks, numAvailTracks, ideo) {
+  var annots, omittedAnnots, m, i, annotsByChr, chr, chrModel, colors,
+    config = ideo.config;
+
   annots = [];
+  omittedAnnots = {};
+
+  colors = colorMap[numAvailTracks - 1];
 
   m = -1;
   for (i = 0; i < rawAnnots.length; i++) {
@@ -64,79 +189,23 @@ function processAnnotData(rawAnnots) {
     m++;
     annots.push({chr: annotsByChr.chr, annots: []});
 
-    for (j = 0; j < annotsByChr.annots.length; j++) {
-      ra = annotsByChr.annots[j];
-      annot = {};
+    [annots, omittedAnnots] =
+      addAnnotsForChr(annots, omittedAnnots, annotsByChr, chr, chrModel,
+        colors, m, keys, numTracks, ideo, i, numAvailTracks);
+  }
 
-      for (k = 0; k < keys.length; k++) {
-        annot[keys[k]] = ra[k];
-      }
+  return [annots, omittedAnnots]
+}
 
-      annot.stop = annot.start + annot.length;
+function sendTrackAndAnnotWarnings(numTracks, omittedAnnots) {
+  var numOmittedTracks;
 
-      startPx = ideo.convertBpToPx(chrModel, annot.start);
-      stopPx = ideo.convertBpToPx(chrModel, annot.stop);
-      px = Math.round((startPx + stopPx) / 2);
-
-      annot.chr = chr;
-      annot.chrIndex = i;
-      annot.px = px;
-      annot.startPx = startPx;
-      annot.stopPx = stopPx;
-
-      if (config.annotationTracks) {
-        // Client annotations, as in annotations-tracks.html
-        annot.trackIndex = ra[3];
-        annotTrack = config.annotationTracks[annot.trackIndex];
-        if (annotTrack.color) {
-          annot.color = annotTrack.color;
-        }
-        if (annotTrack.shape) {
-          annot.shape = annotTrack.shape;
-        }
-        annots[m].annots.push(annot);
-      } else if (keys[3] === 'trackIndex' && numAvailTracks !== 1) {
-        // Sparse server annotations, as in annotations-track-filters.html
-        annot.trackIndex = ra[3];
-        annot.trackIndexOriginal = ra[4];
-        annot.color = '#' + colors[annot.trackIndexOriginal];
-
-        // Catch annots that will be omitted from display
-        if (annot.trackIndex > numTracks - 1) {
-          if (annot.trackIndex in omittedAnnots) {
-            omittedAnnots[annot.trackIndex].push(annot);
-          } else {
-            omittedAnnots[annot.trackIndex] = [annot];
-          }
-          continue;
-        }
-        annots[m].annots.push(annot);
-      } else if (
-        keys.length > 3 &&
-        keys[3] in {trackIndex: 1, color: 1, shape: 1} === false &&
-        keys[4] === 'trackIndexOriginal'
-      ) {
-        // Dense server annotations
-        for (n = 4; n < keys.length; n++) {
-          thisAnnot = Object.assign({}, annot); // copy by value
-          thisAnnot.trackIndex = n - 4;
-          thisAnnot.trackIndexOriginal = n - 3;
-          thisAnnot.color = '#' + colors[thisAnnot.trackIndexOriginal];
-          annots[m].annots.push(thisAnnot);
-        }
-      } else {
-        // Basic client annotations, as in annotations-basic.html
-        // and annotations-external.html
-        annot.trackIndex = 0;
-        if (!annot.color) {
-          annot.color = config.annotationsColor;
-        }
-        if (!annot.shape) {
-          annot.shape = 'triangle';
-        }
-        annots[m].annots.push(annot);
-      }
-    }
+  if (numTracks > 10) {
+    console.error(
+      'Ideogram only displays up to 10 tracks at a time.  ' +
+      'You specified ' + numTracks + ' tracks.  ' +
+      'Perhaps consider a different way to visualize your data.'
+    );
   }
 
   numOmittedTracks = Object.keys(omittedAnnots).length;
@@ -147,20 +216,31 @@ function processAnnotData(rawAnnots) {
       'extra tracks.'
     );
   }
+}
 
-  // Ensure annotation containers are ordered by chromosome
-  unorderedAnnots = annots;
-  annots = [];
-  chrs = ideo.chromosomesArray;
-  for (i = 0; i < chrs.length; i++) {
-    chr = chrs[i].name;
-    for (j = 0; j < unorderedAnnots.length; j++) {
-      annot = unorderedAnnots[j];
-      if (annot.chr === chr) {
-        annots.push(annot);
-      }
-    }
-  }
+/**
+ * Proccesses genome annotation data.
+ *
+ * This method converts raw annotation data from server, which is structured as
+ * an array of arrays, into a more verbose data structure consisting of an
+ * array of objects.  It also adds pixel offset information.
+ */
+function processAnnotData(rawAnnots) {
+  var numTracks, keys, annots, omittedAnnots, numAvailTracks, 
+    ideo = this;
+
+  keys = rawAnnots.keys;
+  rawAnnots = rawAnnots.annots;
+
+  numTracks = ideo.config.numAnnotTracks;
+  numAvailTracks = ideo.numAvailTracks;
+
+  [annots, omittedAnnots] = 
+    addAnnots(rawAnnots, keys, numTracks, numAvailTracks, ideo);
+
+  sendTrackAndAnnotWarnings(numTracks, omittedAnnots)
+
+  annots = orderAnnotContainers(annots, ideo);
 
   return annots;
 }
