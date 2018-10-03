34c6d36865146b21cb85d35f65450c730e97ab03
Eric Weitz <eric.m.weitz@gmail.com>
2018-10-02 23:31:10 -0400

    Begin modularizing centromeres module

--- scripts/python/fetch_chromosomes/centromeres.py
+++ scripts/python/fetch_chromosomes/centromeres.py
@@ -1,3 +1,63 @@
+def update_bands(centromere, bands, new_bands, chr, i, j):
+    cen_start, cen_stop = centromere
+
+    # Extend nearest p-arm band's stop coordinate to the
+    # p_cen's start coordinate (minus 1)
+    cen_start_pre = str(int(cen_start) - 1)
+    new_bands[chr][i - j - 1][3] = cen_start_pre
+    new_bands[chr][i - j - 1][5] = cen_start_pre
+
+    # Extend nearest q-arm band's start coordinate to the
+    # q_cen's stop coordinate (plus 1)
+    cen_stop_post = str(int(cen_stop) + 1)
+    bands[i + j][1] = cen_stop_post
+    bands[i + j][3] = cen_stop_post
+
+    return [bands, new_bands]
+
+
+def get_pcen_and_qcen(centromere, chr):
+    cen_start, cen_stop = centromere
+
+    # Coordinates of the centromere itself
+    cen_mid = int(cen_start) + round((int(cen_stop)-int(cen_start))/2)
+
+    pcen = [
+        'p', 'pcen', cen_start, str(cen_mid - 1),
+        cen_start, str(cen_mid - 1), 'acen'
+    ]
+    qcen = [
+        'q', 'qcen', str(cen_mid), cen_stop,
+        str(cen_mid), cen_stop, 'acen'
+    ]
+    return [pcen, qcen]
+
+
+def get_centromere_parts(centromere, chr, new_bands, bands, band, i, j, pcen_index):
+    band_start, band_stop = band[1:3]
+    cen_start, cen_stop = centromere
+
+    if int(band_stop) < int(cen_start):
+        arm = 'p'
+    else:
+        arm = 'q'
+
+        if int(band_start) < int(cen_stop):
+            # Omit any q-arm bands that start before q-arm pericentromeric band
+            if chr == '1':
+                logger.info('Omit band:')
+                logger.info(band)
+            j += 1
+            return None
+
+        if pcen_index is None:
+            pcen_index = i - j
+            bands, new_bands = update_bands(centromere, bands,
+                new_bands, chr, i, j)
+            pcen, qcen = get_pcen_and_qcen(centromere, chr)
+
+    return [arm, pcen, qcen, pcen_index, new_bands, bands, band, j, pcen_index]
+
 
 def merge_centromeres(bands_by_chr, centromeres, logger_obj):
     """Adds p and q arms to cytobands; thus adds centromere to each chromosome.
@@ -18,59 +78,21 @@ def merge_centromeres(bands_by_chr, centromeres, logger_obj):
         bands = bands_by_chr[chr]
         new_bands[chr] = []
         centromere = centromeres[chr]
-        cen_start, cen_stop = centromere
         pcen_index = None
 
         j = 0
         for i, band in enumerate(bands):
             new_band = band
-            band_start, band_stop = band[1:3]
-            if int(band_stop) < int(cen_start):
-                arm = 'p'
-            else:
-                arm = 'q'
-
-                if int(band_start) < int(cen_stop):
-                    # Omit any q-arm bands that start before q-arm pericentromeric band
-                    if chr == '1':
-                        logger.info('Omit band:')
-                        logger.info(band)
-                    j += 1
-                    continue
-
-                if pcen_index is None:
-                    pcen_index = i - j
-
-                    # Extend nearest p-arm band's stop coordinate to the
-                    # p_cen's start coordinate (minus 1)
-                    cen_start_pre = str(int(cen_start) - 1)
-                    new_bands[chr][i - j - 1][3] = cen_start_pre
-                    new_bands[chr][i - j - 1][5  ] = cen_start_pre
-
-                    # Extend nearest q-arm band's start coordinate to the
-                    # q_cen's stop coordinate (plus 1)
-                    cen_stop_post = str(int(cen_stop) + 1)
-                    bands[i + j][1] = cen_stop_post
-                    bands[i + j][3] = cen_stop_post
-
-                    # Coordinates of the centromere itself
-                    cen_mid = int(cen_start) + round((int(cen_stop)-int(cen_start))/2)
-
-                    pcen = [
-                        'p', 'pcen', cen_start, str(cen_mid - 1),
-                        cen_start, str(cen_mid - 1), 'acen'
-                    ]
-                    qcen = [
-                        'q', 'qcen', str(cen_mid), cen_stop,
-                        str(cen_mid), cen_stop, 'acen'
-                    ]
+            # This is gross.  Can this function be small *and* readable?
+            [arm, pcen, qcen, pcen_index, new_bands, bands, band, j,
+                pcen_index] = get_centromere_parts(centromere, chr, 
+                    new_bands, bands, band, i, j, pcen_index)
             new_band.insert(0, arm)
             new_bands[chr].append(new_band)
         if pcen_index is not None:
             new_bands[chr].insert(pcen_index, qcen)
             new_bands[chr].insert(pcen_index, pcen)
     return new_bands
-
 
 def parse_centromeres(bands_by_chr, logger_obj):
     """Adds p and q arms to cytobands, by parsing embedded centromere bands.
@@ -84,9 +106,7 @@ def parse_centromeres(bands_by_chr, logger_obj):
     centromere.
     """
     global logger
-
     logger = logger_obj
-
     logger.info('Entering parse_centromeres')
 
     # If centromeres aren't embedded in the input banding data,
