41a4e7d76b04262030db32257f655e689677f97b
Eric Weitz <eric.m.weitz@gmail.com>
2018-11-25 10:19:08 -0500

    Simplify, generalize legend interface

--- src/js/annotations/legend.js
+++ src/js/annotations/legend.js
@@ -4,69 +4,57 @@ var d3 = Object.assign({}, d3selection);
 
 var lineHeight = 19;
 
-function getListItems(labels, svg, hasDisplayName, heatmap) {
-  var j, threshold, color, label, y, rectAttrs;
+function getListItems(labels, svg, hasName, list) {
+  var i, color, y, rectAttrs, row;
 
   rectAttrs = 'height="10" width="10"  y="5" style="stroke: #888;"';
 
-  for (j = 0; j < heatmap.thresholds.length; j++) {
-    threshold = heatmap.thresholds[j];
-    y = j * lineHeight;
-    if (hasDisplayName) y += lineHeight;
-    color = '<rect ' + rectAttrs + ' fill="' + threshold[1] + '"/>';
-    label = '<li>' + threshold[2] + '</li>';
-    svg += '<g transform="translate(0, ' + y + ')">' + color + '</g>'
-    labels += label;
+  for (i = 0; i < list.rows.length; i++) {
+    row = list.rows[i];
+    labels += '<li>' + row.name + '</li>';
+    y = lineHeight * i;
+    if (hasName) y += lineHeight;
+    color = '<rect ' + rectAttrs + ' fill="' + row.color + '"/>';
+    svg += '<g transform="translate(0, ' + y + ')">' + color + '</g>';
   }
 
   return [labels, svg];
 }
 
-function getHeader(heatmap) {
-  var labels, hasDisplayName;
+function getHeader(list) {
+  var labels, hasName;
   labels = '';
-  hasDisplayName = false;
-  if ('displayName' in heatmap) {
+  hasName = false;
+  if ('name' in list) {
     labels +=
       '<span style="position: relative; left: -15px;">' +
-        heatmap.displayName +
+        list.name +
       '</span>';
-    hasDisplayName = true;
+    hasName = true;
   }
-  return [labels, hasDisplayName];
+  return [labels, hasName];
 }
 
-function writeHeatmapLegend(ideo) {
-  var heatmaps, thresholdHasLabel, i, heatmap, legend, hasDisplayName, svg,
-    labels, legendStyle;
+function writeLegend(ideo) {
+  var i, legend, hasName, svg, labels, legendStyle, list, content;
 
-  heatmaps = ideo.config.heatmaps;
-  thresholdHasLabel = heatmaps[0].thresholds[0].length > 2;
-  if (!thresholdHasLabel) return;
-
-  legend = '';
+  legend = ideo.config.legend;
+  content = '';
   legendStyle = 'position: relative; top: -11px; left: -14px;';
 
-  for (i = 0; i < heatmaps.length; i++) {
-    heatmap = heatmaps[i];
-    [labels, hasDisplayName] = getHeader(heatmap);
+  for (i = 0; i < legend.length; i++) {
+    list = legend[i];
+    [labels, hasName] = getHeader(list);
     svg = '<svg width="' + lineHeight + '" style="float: left">';
-    [labels, svg] = getListItems(labels, svg, hasDisplayName, heatmap);
+    [labels, svg] = getListItems(labels, svg, hasName, list);
     svg += '</svg>'
-    legend += svg + '<ul style="' + legendStyle + '">' + labels + '</ul>';
+    content += svg + '<ul style="' + legendStyle + '">' + labels + '</ul>';
   }
 
   d3.select(ideo.config.container + ' #_ideogramOuterWrap')
     .append('div')
       .attr('id', '_ideogramLegend')
-      .html(legend);
-}
-
-function writeLegend(ideo) {
-  var config = ideo.config;
-  if (config.heatmaps) {
-    writeHeatmapLegend(ideo);
-  }
+      .html(content);
 }
 
 export { writeLegend }
