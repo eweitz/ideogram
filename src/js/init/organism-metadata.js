d34640813961a0176e6107fea9776895fbfce7b8
Eric Weitz <eric.m.weitz@gmail.com>
2019-04-22 07:54:45 -0400

    Remove hardcoded scientificNamAbbr field

--- src/js/init/organism-metadata.js
+++ src/js/init/organism-metadata.js
@@ -2,7 +2,6 @@ var organismMetadata = {
   9606: {
     commonName: 'Human',
     scientificName: 'Homo sapiens',
-    scientificNameAbbr: 'H. sapiens',
     assemblies: {
       default: 'GCF_000001405.26', // GRCh38
       GRCh38: 'GCF_000001405.26',
@@ -12,7 +11,6 @@ var organismMetadata = {
   10090: {
     commonName: 'Mouse',
     scientificName: 'Mus musculus',
-    scientificNameAbbr: 'M. musculus',
     assemblies: {
       default: 'GCF_000001635.20', // GRCm38
       GRCm38: 'GCF_000001635.20',
@@ -20,9 +18,8 @@ var organismMetadata = {
     }
   },
   3702: {
-    commonName: 'Arabidopsis thaliana',
+    commonName: 'Thale cress',
     scientificName: 'Arabidopsis thaliana',
-    scientificNameAbbr: 'A. thaliana',
     assemblies: {
       default: 'GCF_000001735.3', // TAIR10
       TAIR10: 'GCF_000001735.3'
@@ -31,7 +28,6 @@ var organismMetadata = {
   4641: {
     commonName: 'banana',
     scientificName: 'Musa acuminata',
-    scientificNameAbbr: 'M. acuminata',
     assemblies: {
       default: 'mock'
     }
