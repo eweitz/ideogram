5b914588d81a06355ef2e67d828136110cb7b019
Eric Weitz <eric.m.weitz@gmail.com>
2020-01-04 22:10:52 -0500

    Document DEG matrix format, relabel "factor" to "group"

--- scripts/python/deg_to_ideogram.py
+++ scripts/python/deg_to_ideogram.py
@@ -1,7 +1,21 @@
 '''Convert differentially expressed gene (DEG) matrix into Ideogram annotations
 
+This pipeline converts a DEG matrix file containing gene expression summary
+statistics into an Ideogram.js annotations JSON file.  The resulting JSON file
+is used for exploratory data analysis as demonstrated at:
+
+https://eweitz.github.io/ideogram/differential-expression
+
+The DEG matrix has several groups of columns, with headers ordered like so:
+
+* metadata: "REFSEQ","SYMBOL","GENENAME","ENSEMBL","ENTREZID","STRING_id","GOSLIM_IDS"
+* replicates: <sample_prefix>-<group>-<replicate_number>, e.g. "Mmus-C57-6T-TMS-FLT-Rep1"
+* stats: "All.mean","All.stdev","F.p.value"
+* stats_by_group: Group.<stat>_(<group>), e.g. "Group.Mean_(Space Flight)"
+* stats_by_comparison: <stat>_(<group1>)v(<group2>), e.g. "Log2fc_(Ground Control)v(Space Flight)"
+
 Example call:
-python3 deg_to_ideogram.py --gene-pos-path TODO --deg-path TODO
+python3 deg_to_ideogram.py --gen-pos-path TODO --deg-path TODO
 '''
 
 import argparse
@@ -11,8 +25,8 @@ import csv
 parser = argparse.ArgumentParser(
     description=__doc__,
     formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('--gene-pos-path',
-                    help='Path to input gene position file')
+parser.add_argument('--gen-pos-path',
+                    help='Path to input gene position file built by reduce_gtf.py')
 parser.add_argument('--deg-path',
                     help='Path to input differentially expressed gene (DEG) matrix')
 parser.add_argument('--output-dir',
@@ -20,7 +34,7 @@ parser.add_argument('--output-dir',
                     default='ideogram/data/annotations/')
 
 args = parser.parse_args()
-gene_pos_path = args.gene_pos_path
+gene_pos_path = args.gen_pos_path
 deg_path = args.deg_path
 output_dir = args.output_dir
 
@@ -69,9 +83,9 @@ def get_gene_coordinates(gene_pos_path):
     return [gene_coordinates, gene_types, gene_pos_metadata]
 
 def get_comparisons(headers):
-    """Return indices of comparisons by factor, and names of comparisons
+    """Return indices of comparisons by group, and names of comparisons
 
-    A factor is something like "Space Flight" or "Ground Control".
+    A group is something like "Space Flight" or "Ground Control".
     """
     # Headers for numeric expression values
     for i, header in enumerate(headers):
@@ -79,30 +93,30 @@ def get_comparisons(headers):
             log2fc_index = i
             break
 
-    comparisons_by_factor = {}
+    comparisons_by_group = {}
 
     for i, header in enumerate(headers[log2fc_index:]):
         if ')v(' not in header: continue # not a comparison, so skip
         metric = header.split('_')[0] # e.g. Log2fc, P.value, or Adj.p.value
-        factors = []
+        groups = []
         comparison = header.replace(metric + '_', '').strip('(').strip(')')
-        for factor in comparison.split(')v('):
-            factors.append(factor)
+        for group in comparison.split(')v('):
+            groups.append(group)
 
         index = log2fc_index + i
 
-        factor_1 = slug(factors[0])
-        if factor_1 in comparisons_by_factor:
-            comparisons_by_factor[factor_1]['indices'].append(index)
-            comparisons_by_factor[factor_1]['comparisons'].append(header)
+        group_1 = slug(groups[0])
+        if group_1 in comparisons_by_group:
+            comparisons_by_group[group_1]['indices'].append(index)
+            comparisons_by_group[group_1]['comparisons'].append(header)
         else:
-            comparisons_by_factor[factor_1] = {
+            comparisons_by_group[group_1] = {
                 'indices': [index],
                 'comparisons': [header],
-                'label': factors[0]
+                'label': groups[0]
             }
 
-    return comparisons_by_factor
+    return comparisons_by_group
 
 def parse_deg_matrix(deg_matrix_path):
     """Get gene metadata and expression values from DEG matrix
@@ -117,26 +131,26 @@ def parse_deg_matrix(deg_matrix_path):
         headers = next(reader, None)
         metadata_keys = [headers[2], headers[4]] # GENENAME, ENTREZID
 
-        comparisons_by_factor = get_comparisons(headers)
-        for factor in comparisons_by_factor:
-            gene_stats[factor] = {}
+        comparisons_by_group = get_comparisons(headers)
+        for group in comparisons_by_group:
+            gene_stats[group] = {}
 
         for row in reader:
             gene_symbol = row[1]
             gene_metadata[gene_symbol] = [row[2], row[4]]
-            for factor in comparisons_by_factor:
-                comparison_indices = comparisons_by_factor[factor]['indices']
-                gene_stats[factor][gene_symbol] = [row[i] for i in comparison_indices]
+            for group in comparisons_by_group:
+                comparison_indices = comparisons_by_group[group]['indices']
+                gene_stats[group][gene_symbol] = [row[i] for i in comparison_indices]
 
     metadata_labels = get_key_labels(metadata_keys)
     metadata_list = list(metadata_labels.keys())
     
-    return [gene_metadata, metadata_list, gene_stats, comparisons_by_factor]
+    return [gene_metadata, metadata_list, gene_stats, comparisons_by_group]
 
 def get_annots_by_chr(gene_coordinates, gene_expressions, gene_types, gene_metadata):
     """Merge coordinates and expressions, return Ideogram annotations
     """
-    annots_by_chr_by_factor = {}
+    annots_by_chr_by_group = {}
 
     # Some gene types (e.g. pseudogenes) exist in the genome annotation,
     # but are never expressed.  Don't count these.
@@ -145,8 +159,8 @@ def get_annots_by_chr(gene_coordinates, gene_expressions, gene_types, gene_metad
         gene_symbol = coordinates['symbol']
         gene_type = coordinates['type']
         lacks_gene_type = True
-        for factor in gene_expressions:
-            if gene_symbol not in gene_expressions[factor]:
+        for group in gene_expressions:
+            if gene_symbol not in gene_expressions[group]:
                 lacks_gene_type = False
         if lacks_gene_type is False:
             gene_types[gene_type] -= 1
@@ -168,16 +182,16 @@ def get_annots_by_chr(gene_coordinates, gene_expressions, gene_types, gene_metad
         stop = int(coordinates['stop'])
         length = stop - start
 
-        for factor in gene_expressions:
-            if symbol not in gene_expressions[factor]:
+        for group in gene_expressions:
+            if symbol not in gene_expressions[group]:
                 continue
 
-            if factor not in annots_by_chr_by_factor:
-                annots_by_chr_by_factor[factor] = {}
-            if chr not in annots_by_chr_by_factor[factor]:
-                annots_by_chr_by_factor[factor][chr] = {'chr': chr, 'annots': []}
+            if group not in annots_by_chr_by_group:
+                annots_by_chr_by_group[group] = {}
+            if chr not in annots_by_chr_by_group[group]:
+                annots_by_chr_by_group[group][chr] = {'chr': chr, 'annots': []}
 
-            expressions = gene_expressions[factor][symbol]
+            expressions = gene_expressions[group][symbol]
     
             annot = [
                 symbol,
@@ -193,12 +207,9 @@ def get_annots_by_chr(gene_coordinates, gene_expressions, gene_types, gene_metad
                     exp_value = -1
                 annot.append(round(float(exp_value), 3))
 
-            annots_by_chr_by_factor[factor][chr]['annots'].append(annot)
+            annots_by_chr_by_group[group][chr]['annots'].append(annot)
 
-    print('annots_by_chr_by_factor.keys()')
-    print(annots_by_chr_by_factor.keys())
-
-    return [annots_by_chr_by_factor, sorted_gene_types]
+    return [annots_by_chr_by_group, sorted_gene_types]
 
 def slug(value):
     return value.lower()\
@@ -234,68 +245,68 @@ def get_metadata(gene_pos_metadata, sorted_gene_types, key_labels):
     return metadata
 
 
-def get_deg_metadata(factor, annots_by_chr_by_factor):
+def get_deg_metadata(group, annots_by_chr_by_group):
 
-    factors = list(annots_by_chr_by_factor.keys())
+    groups = list(annots_by_chr_by_group.keys())
 
-    split_factor = factor.split('-')
+    split_group = group.split('-')
 
-    if 'space-flight' in factor:
+    if 'space-flight' in group:
         suffix = ''
-        other_factor = 'ground-control'
-    elif 'flt' in factor and 'rr' not in factor.lower():
+        other_group = 'ground-control'
+    elif 'flt' in group and 'rr' not in group.lower():
         suffix = ''
-        if len(split_factor) > 2:
-            other_factor = '-'.join(split_factor[:2]) + '-gc'
+        if len(split_group) > 2:
+            other_group = '-'.join(split_group[:2]) + '-gc'
         else:
             # Seen in GLDS-242 (e.g. "FLT-C1")
-            other_factor = 'bsl-' + split_factor[1]
-    elif factor == 'rr1-flt-wercc':
+            other_group = 'bsl-' + split_group[1]
+    elif group == 'rr1-flt-wercc':
         # Seen in GLDS-168
-        other_factor = 'rr1-bsl-wercc'
+        other_group = 'rr1-bsl-wercc'
         suffix = ''
     else:
-        suffix = '_' + factor
-        factor_index = factors.index(factor) 
-        if factor_index < len(factors) - 1:
-            other_index = factor_index + 1
+        suffix = '_' + group
+        group_index = groups.index(group) 
+        if group_index < len(groups) - 1:
+            other_index = group_index + 1
         else:
-            other_index = factor_index - 1
-        other_factor = factors[other_index]
+            other_index = group_index - 1
+        other_group = groups[other_index]
 
     deg_metadata = {
-        'factors': factors,
-        'thisFactor': factor,
-        'otherFactor': other_factor
+        'groups': groups,
+        'thisgroup': group,
+        'othergroup': other_group
     }
 
     return deg_metadata, suffix
 
 
 coordinates, gene_types, gene_pos_metadata = get_gene_coordinates(gene_pos_path)
-metadata, metadata_list, expressions, comparisons_by_factor = parse_deg_matrix(deg_path)
+metadata, metadata_list, expressions, comparisons_by_group = parse_deg_matrix(deg_path)
 
-[annots_by_chr_by_factor, sorted_gene_types] =\
+[annots_by_chr_by_group, sorted_gene_types] =\
     get_annots_by_chr(coordinates, expressions, gene_types, metadata)
 
 content_by_suffix = {}
 
-for i, factor in enumerate(annots_by_chr_by_factor):
-    annots_by_chr = annots_by_chr_by_factor[factor]
+for i, group in enumerate(annots_by_chr_by_group):
+    annots_by_chr = annots_by_chr_by_group[group]
     annots_list = list(annots_by_chr.values())
 
-    comparisons = comparisons_by_factor[factor]['comparisons']
+    comparisons = comparisons_by_group[group]['comparisons']
     comparison_labels = get_key_labels(comparisons)
     comparison_list = list(comparison_labels.keys())
 
     keys = ['name', 'start', 'length', 'gene-type'] + metadata_list + comparison_list
 
     metadata = get_metadata(gene_pos_metadata, sorted_gene_types, comparison_labels)
-    for factor2 in annots_by_chr_by_factor:
-        factor_label = comparisons_by_factor[factor2]['label']
-        metadata['labels'][factor2] = factor_label
+    for group2 in annots_by_chr_by_group:
+        group_label = comparisons_by_group[group2]['label']
+        metadata['labels'][group2] = group_label
 
-    deg_metadata, suffix = get_deg_metadata(factor, annots_by_chr_by_factor)
+    deg_metadata, suffix = get_deg_metadata(group, annots_by_chr_by_group)
 
     metadata['deg'] = deg_metadata
 
@@ -311,7 +322,7 @@ if '' not in content_by_suffix:
     print(content_by_suffix.keys())
     for suffix in content_by_suffix:
         if 'control' not in suffix:
-            # Try to find a suffix indicating strongest treatment factor
+            # Try to find a suffix indicating strongest treatment group
             default_suffix = suffix
             break
     if default_suffix is None:
