79245b5bc6f02c1c62f65f26c16f9eccc7bfc568
Eric Weitz <eric.m.weitz@gmail.com>
2020-10-04 07:07:59 -0400

    Remove explicit dataDir from React example

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -23,7 +23,6 @@ class AppIdeogram extends Component {
   componentDidMount() {
     return new Ideogram({
       organism: 'human',
-      dataDir: 'https://unpkg.com/ideogram@0.13.0/dist/data/bands/native/',
       container: '#ideo-container'
     });
   }
