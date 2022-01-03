b10fa003a702b3faf40f08f339dfb87d7691d7d7
Eric Weitz <eric.m.weitz@gmail.com>
2022-01-03 18:47:35 -0500

    Determine type of interaction using WikiPathways GPML data

--- src/js/kit/wikipathways.js
+++ src/js/kit/wikipathways.js
@@ -1,3 +1,23 @@
+// Definitions for ArrowHead values in WikiPathways GPML
+//
+// See also: https://discover.nci.nih.gov/mim/formal_mim_spec.pdf
+const interactionArrowMap = {
+  'Arrow': ['acts on', 'acted on by'],
+  'TBar': ['inhibits', 'is inhibited by'],
+  'mim-binding': ['binds', 'binds'],
+  'mim-catalysis': ['catalyzes', 'catalyzed by'],
+  'mim-cleavage': ['cleaves', 'cleaved by'],
+  'mim-conversion': ['converts', 'converted by'],
+  // 'mim-covalent-bond': ['covalently binds',
+  // 'mim-gap': 'MimGap',
+  'mim-inhibition': ['inhibits', 'is inhibited by'],
+  'mim-modification': ['modifies', 'is modified by'],
+  'mim-necessary-stimulation':
+    ['necessarily stimulates', 'is necessarily stimulated by'],
+  'mim-stimulation': ['stimulates', 'is stimulated by'],
+  'mim-transcription-translation':
+    ['transcribes / translates', 'is transcribed / translated by']
+};
 
 /**
  * Fetch GPML for pathway and find ID of Interaction between two genes,
@@ -8,7 +28,7 @@
  * fetches augmented GPML data for the pathway, and queries it to get only
  * interactions between the two genes.
  */
- async function findGpmlConnections(gene1, gene2, pathwayId) {
+ async function findGpmlConnections(searchedGene, interactingGene, pathwayId) {
   const wpBaseUrl = 'https://webservice.wikipathways.org/';
   const pathwayUrl = wpBaseUrl + `getPathway?pwId=${pathwayId}&format=json`;
   const wpResponse = await fetch(pathwayUrl);
@@ -20,11 +40,12 @@
   // console.log('gpml', gpml)
   // console.log('gene1', gene1)
   const nodes = gpml.querySelectorAll(
-    `DataNode[TextLabel="${gene1}"],` +
-    `DataNode[TextLabel="${gene2}"]`
+    `DataNode[TextLabel="${searchedGene}"],` +
+    `DataNode[TextLabel="${interactingGene}"]`
   );
 
   const genes = Array.from(nodes).map(node => {
+    console.log('node.innerHTML', node.innerHTML);
     return {
       textLabel: node.getAttribute('TextLabel'),
       graphId: node.getAttribute('GraphId'),
@@ -62,8 +83,14 @@
   Array.from(graphicsXml).forEach(graphics => {
     const endGraphRefs = [];
     let numMatchingPoints = 0;
+    let ixnType = null;
+    // console.log('graphics', graphics)
+    let searchedGeneIndex = null;
     Array.from(graphics.children).forEach(child => {
-      if (child.nodeName !== 'Point') return;
+      if (child.nodeName !== 'Point') {
+        console.log('child.nodeName', child.nodeName);
+        return;
+      }
       const point = child;
       const graphRef = point.getAttribute('GraphRef');
       // const group = point.getAttribute('GraphRef');
@@ -73,6 +100,17 @@
       if (matchingGraphIds.includes(graphRef)) {
         numMatchingPoints += 1;
         endGraphRefs.push(graphRef);
+        if (point.getAttribute('ArrowHead')) {
+          const arrowHead = point.getAttribute('ArrowHead');
+          const pointLabel = genes.find(g => g.graphId = graphRef).textLabel;
+          const isStart = pointLabel === searchedGene;
+          if (searchedGeneIndex === null) {
+            searchedGeneIndex = isStart ? 0 : 1;
+          }
+          console.log('pointLabel', pointLabel)
+          console.log('searchedGene', searchedGene)
+          ixnType = interactionArrowMap[arrowHead][isStart ? 0 : 1];
+        }
       }
       // if (numMatchingPoints > 0) {
       //   console.log('graphics.parentNode', graphics.parentNode)
@@ -80,10 +118,19 @@
     });
     // console.log('numMatchingPoints', numMatchingPoints)
     if (numMatchingPoints >= 2) {
+      if (searchedGeneIndex === null) {
+        const gi = genes.indexOf(genes.find(g => g.textLabel = searchedGene));
+        searchedGeneIndex = gi;
+        ixnType = 'interacts with';
+        console.log('searchedGeneIndex', searchedGeneIndex)
+      }
       const interactionGraphId = graphics.parentNode.getAttribute('GraphId');
       const connection = {
         'interactionId': interactionGraphId,
-        'endIds': endGraphRefs
+        'endIds': endGraphRefs,
+        ixnType,
+        genes,
+        searchedGeneIndex // Whether searched gene is at start or end
       };
       connections.push(connection);
     }
@@ -98,7 +145,9 @@
 export async function fetchInteractionDiagram(annot, descObj, ideo) {
   // Fetch raw SVG for pathway diagram
   const pathwayId = descObj.pathwayIds[0];
+  // const baseUrl = 'https://eweitz.github.io/cachome/wikipathways/';
   const baseUrl = 'https://cachome.github.io/wikipathways/';
+  // const baseUrl = 'http://localhost/wikipathways/data/';
   const diagramUrl = baseUrl + pathwayId + '.svg';
   const response = await fetch(diagramUrl);
   if (response.ok) {
@@ -107,11 +156,15 @@ export async function fetchInteractionDiagram(annot, descObj, ideo) {
       Object.entries(ideo.annotDescriptions.annots)
         .find(([k, v]) => v.type === 'searched gene')[0];
     // console.log('searchedGene', searchedGene)
-    const cxns = await findGpmlConnections(annot.name, searchedGene, pathwayId);
+    const cxns = await findGpmlConnections(searchedGene, annot.name, pathwayId);
 
     let selectors = `[name=${annot.name}]`;
+    let searchedGeneIndex = 0;
+    let interactingGeneIndex;
     if (cxns.length > 0) {
       selectors = cxns[0].endIds.map(id => '#' + id).join(',');
+      searchedGeneIndex = cxns[0].searchedGeneIndex;
+      interactingGeneIndex = (searchedGeneIndex === 0) ? 1 : 0;
     }
     // https://webservice.wikipathways.org/findInteractions?query=ACE2&format=json
 
@@ -127,11 +180,16 @@ export async function fetchInteractionDiagram(annot, descObj, ideo) {
       .attr('width', 350)
       .attr('height', 300);
 
+    const viewport = document.querySelector('.svg-pan-zoom_viewport');
+    viewport.removeAttribute('style');
+    viewport.removeAttribute('transform');
+
     const matches = document.querySelectorAll(selectors);
     console.log('matches', matches)
-    const m0 = matches[0].getCTM();
-    const m0Rect = matches[0].getBoundingClientRect();
-    const m0Box = matches[0].getBBox();
+    const match0 = matches[searchedGeneIndex]
+    const m0 = match0.getCTM();
+    const m0Rect = match0.getBoundingClientRect();
+    const m0Box = match0.getBBox();
     const m0MinX = m0.e/m0.a;
     const m0MinY = m0.f/m0.d;
 
@@ -143,29 +201,36 @@ export async function fetchInteractionDiagram(annot, descObj, ideo) {
     width = 350;
     height = 300;
 
-    matches[0].children[0].setAttribute('fill', '#F55');
+    // matches[0].children[0].setAttribute('fill', '#F55');
+    // console.log('matches[0].children[0].style', matches[0].children[0].style)
+    match0.children[0].style.fill = '#f55';
 
     if (matches.length > 1) {
-      matches[1].children[0].setAttribute('fill', '#C4C');
-      // const m1 = matches[1].getCTM();
-      // const m1Rect = matches[1].getBoundingClientRect();
-      // const m1Box = matches[1].getBBox();
-      // console.log('m0', m0)
-      // console.log('m1', m1)
-      // const m1MinX = m1.e/m1.a;
-      // const m1MinY = m1.f/m1.d;
-      // // const m1MinX = m1.e/m1.a + m1Rect.width;
-      // // const m1MinY = m1.f/m1.d - m1Rect.height;
-      // if (m1MinX < m0MinX) minX = m1MinX;
-      // if (m1MinY < m0MinY) minY = m1MinY;
+      // console.log('matches.length > 1')
+      // matches[1].children[0].setAttribute('fill', '#C4C');
+      const match1 = matches[interactingGeneIndex];
+      // console.log('match1')
+      // console.log(match1)
+      match1.children[0].style.fill = '#c4c';
+      const m1 = matches[1].getCTM();
+      const m1Rect = matches[1].getBoundingClientRect();
+      const m1Box = matches[1].getBBox();
+      console.log('m0', m0)
+      console.log('m1', m1)
+      const m1MinX = m1.e/m1.a;
+      const m1MinY = m1.f/m1.d;
+      // const m1MinX = m1.e/m1.a + m1Rect.width;
+      // const m1MinY = m1.f/m1.d - m1Rect.height;
+      if (m1MinX < m0MinX) minX = m1MinX;
+      if (m1MinY < m0MinY) minY = m1MinY;
 
-      // let pairWidth = 0;
-      // if (m0Rect.left < m1Rect.left) {
-      //   // pairWidth = m1Rect.right - m0Rect.left;
-      //   width += m1Box.width + 40;
-      // }
+      let pairWidth = 0;
+      if (m0Rect.left < m1Rect.left) {
+        // pairWidth = m1Rect.right - m0Rect.left;
+        width += m1Box.width + 40;
+      }
 
-      // // width += pairWidth;
+      // width += pairWidth;
 
       // console.log('m0Rect', m0Rect)
       // console.log('m1Rect', m1Rect)
@@ -175,18 +240,23 @@ export async function fetchInteractionDiagram(annot, descObj, ideo) {
       // console.log('m0MinY', m0MinY)
       // console.log('pairWidth', pairWidth)
       // console.log('width', width)
-      // // width += Math.abs(m1MinX - m0MinX);
-      // // height += Math.abs(m1MinY - m0MinY);
+      // width += Math.abs(m1MinX - m0MinX);
+      // height += Math.abs(m1MinY - m0MinY);
 
       // minX -= 100;
       // minY -= 100;
 
-      minX -= 150;
-      minY -= 150;
+      // minX -= 150;
+      // minY -= 150;
     } else {
       minX -= 150;
       minY -= 150;
     }
+
+    minX = Math.round(minX);
+    minY = Math.round(minY);
+    width = Math.round(width);
+    height = Math.round(height);
 
     const viewBox = `${minX} ${minY} ${width} ${height}`;
     console.log('viewBox', viewBox);
