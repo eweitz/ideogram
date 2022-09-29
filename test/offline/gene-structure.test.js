f453b533cffeca7f11fca215a0ddd8e090215e71
Eric Weitz <eric.m.weitz@gmail.com>
2022-09-28 21:51:01 -0400

    Test subparts for negative-stranded gene

--- test/offline/gene-structure.test.js
+++ test/offline/gene-structure.test.js
@@ -47,10 +47,12 @@ describe('Ideogram gene structure functionality', function() {
 
   it('supports mouse-highlighting and keyboard-navigating subparts', done => {
     async function callback() {
+
+      // Positive-stranded gene
       await ideogram.plotRelatedGenes('APOE');
       setTimeout(async function() {
-        const apoelLabel = document.querySelector('#ideogramLabel__c18_a1');
-        apoelLabel.dispatchEvent(new Event('mouseover'));
+        const apoeLabel = document.querySelector('#ideogramLabel__c18_a1');
+        apoeLabel.dispatchEvent(new Event('mouseover'));
 
         // Mimic real user action
         const container =
@@ -58,21 +60,45 @@ describe('Ideogram gene structure functionality', function() {
         container.dispatchEvent(new Event('mouseenter'));
 
         // Navigate by hovering with cursor
-        const subparts = document.querySelectorAll('rect.subpart');
+        let subparts = document.querySelectorAll('rect.subpart');
         const exon3 = subparts[4];
         exon3.dispatchEvent(new Event('mouseenter')); // hover over exon
-        const footer = document.querySelector('._ideoGeneStructureFooter');
+        let footer = document.querySelector('._ideoGeneStructureFooter');
         const exonText = footer.textContent.slice(0, 20);
         assert.equal(exonText, 'Exon 3 of 4 | 193 bp');
 
         // Navigate subparts by pressing arrow key
         const left = new KeyboardEvent('keydown', {key: 'ArrowLeft'});
         document.dispatchEvent(left);
-        console.log(footer.textContent)
         const exonText2 = footer.textContent;
         assert.equal(
           exonText2,
           'Exon 2 of 4 | 66 bp' +
+          'Transcript name: APOE-201' +
+          'Exons: 4 | Biotype: protein coding | Strand: +'
+        );
+
+        // Negative-stranded gene
+        const apoa1Label = document.querySelector('#ideogramLabel__c10_a2');
+        apoa1Label.dispatchEvent(new Event('mouseover'));
+
+        // Mimic real user action
+        container.dispatchEvent(new Event('mouseenter'));
+
+        // Navigate by hovering with cursor
+        subparts = document.querySelectorAll('rect.subpart');
+        const exon4 = subparts[0];
+        exon4.dispatchEvent(new Event('mouseenter')); // hover over exon
+        footer = document.querySelector('._ideoGeneStructureFooter');
+        const exon4Text = footer.textContent.slice(0, 20);
+        assert.equal(exon4Text, 'Exon 4 of 4 | 661 bp');
+
+        // Navigate subparts by pressing arrow key
+        document.dispatchEvent(left);
+        const exon3Text = footer.textContent;
+        assert.equal(
+          exon3Text,
+          'Exon 3 of 4 | 157 bp' +
           'Transcript name: APOE-201' +
           'Exons: 4 | Biotype: protein coding | Strand: +'
         );
