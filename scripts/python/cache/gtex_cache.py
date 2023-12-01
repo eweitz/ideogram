858fa9a2784494428a90ae0641a5cef8d6b027d5
Eric Weitz <eric.m.weitz@gmail.com>
2023-11-30 20:43:56 -0500

    Add pipeline for top tissues by gene, per GTEx

--- scripts/python/cache/gtex_cache.py
+++ scripts/python/cache/gtex_cache.py
@@ -1,5 +1,16 @@
 import urllib.request
+import csv
 import json
+import os
+import sys
+import urllib.parse
+
+# Enable importing local modules when directly calling as script
+if __name__ == "__main__":
+    cur_dir = os.path.join(os.path.dirname(__file__))
+    sys.path.append(cur_dir + "/..")
+
+from lib import download_gzip
 
 base_url = 'https://gtexportal.org/api/v2'
 
@@ -77,4 +88,53 @@ def process_top_genes_by_tissue():
     output_path = 'cache/gtex_top_genes_by_tissue.json'
     write_json_file(output, output_path)
 
-process_top_genes_by_tissue()
+def process_top_tissues_by_gene():
+    """Make JSON file of top 3 tissues (by expression in GTEx) for each gene
+    """
+
+    # To manually fetch GTEx file:
+    # 1.  Go to https://www.gtexportal.org/home/downloads/adult-gtex#bulk_tissue_expression
+    # 2.  Note the "GTEx Analysis V8" section, and "RNA-Seq" table shown by default
+    # 3.  Find the row that says:
+    #     "GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct.gz"
+    #     and notes the file size of "6.6 MB"
+    # 4.  Click the download button.  This source data is public.
+    gct_path = "GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_median_tpm.gct"
+    url = (
+        "https://storage.googleapis.com/storage/v1/b/adult-gtex/o/" +
+        urllib.parse.quote_plus(f"bulk-gex/v8/rna-seq/{gct_path}.gz") +
+        "?alt=media"
+    )
+    download_gzip(url, gct_path, cache=True)
+
+    top_tissues_by_gene = {}
+
+    with open(gct_path) as file:
+        reader = csv.reader(file, delimiter="\t")
+
+        for i, row in enumerate(reader):
+            if i < 3:
+                continue
+
+            gene_name = row[1]
+
+            tissue_expressions = []
+            expressions = [float(expression) for expression in row[2:]]
+
+            for j, median_expression in enumerate(expressions):
+                if median_expression > 0:
+                    tissue_id = j
+                    tissue_expressions.append([tissue_id, median_expression])
+
+            sorted_tissue_expressions = sorted(tissue_expressions, key=lambda e: e[1])
+            sorted_tissues = [e[0] for e in sorted_tissue_expressions]
+            top_tissues = sorted_tissues[:3]
+
+            top_tissues_by_gene[gene_name] = top_tissues
+
+    output_path = 'cache/gtex_top_tissues_by_gene.json'
+    write_json_file(top_tissues_by_gene, output_path)
+
+# process_top_genes_by_tissue()
+
+process_top_tissues_by_gene()
