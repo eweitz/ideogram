3fa1658e4c4ffceb581392b07fd7399fed1457dd
Eric Weitz <eric.m.weitz@gmail.com>
2023-12-05 20:17:19 -0500

    Add tissue cache worker

---
+++ src/js/init/caches/tissue-cache-worker.js
@@ -0,0 +1,75 @@
+import {fetchAndParse, getFullId, inspectWorker} from './cache-lib';
+
+
+function parseTissueKeys(rawTissuesString) {
+  const tissueNames = [];
+  const tissueColors = [];
+  rawTissuesString.split(';').forEach(nameAndColor => {
+    nameAndColor = nameAndColor.split(',');
+    tissueNames.push(nameAndColor[0]);
+    tissueColors.push(nameAndColor[1]);
+  });
+  return [tissueNames, tissueColors];
+}
+
+/** Transform a stringified list of integers into an actual list of integers */
+function processIds(ids) {
+  const processedIds = [];
+  const splitIds = ids.split(',');
+  for (let i = 0; i < splitIds.length; i++) {
+    processedIds.push(parseInt(splitIds[i], 10));
+  }
+  return processedIds;
+}
+
+/** Parse a tissue cache TSV file */
+export function parseTissueCache(rawTsv, perfTimes) {
+  console.log('in parseTissueCache')
+  const tissueIdsByName = {};
+  let tissueNames;
+  let tissueColors;
+
+  let t0 = performance.now();
+  const lines = rawTsv.split(/\r\n|\n/);
+  perfTimes.rawTsvSplit = Math.round(performance.now() - t0);
+
+  t0 = performance.now();
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line === '') continue; // Skip empty lines
+    if (line[0] === '#') {
+      if (line.slice(0, 10) === '## tissues') {
+        const parsedTissueKeys = parseTissueKeys(line.split('tissues: ')[1]);
+        tissueNames = parsedTissueKeys[0];
+        tissueColors = parsedTissueKeys[1];
+      }
+      continue;
+    }
+    const columns = line.trim().split(/\t/);
+    const [
+      gene, rawTissueIds, rawTopTissueIds
+    ] = columns;
+
+    const tissueIds = processIds(rawTissueIds);
+    const topTissueIds = rawTopTissueIds ? processIds(rawTopTissueIds) : [];
+
+    tissueIdsByName[gene] = [tissueIds, topTissueIds];
+  };
+  const t1 = performance.now();
+  perfTimes.parseCacheLoop = Math.round(t1 - t0);
+
+  return {
+    tissueIdsByName,
+    tissueNames,
+    tissueColors
+  };
+}
+
+// Uncomment when workers work outside localhost
+// addEventListener('message', async event => {
+//   console.time('tissueCacheWorker');
+//   const [cacheUrl, perfTimes, debug] = event.data;
+//   const result = await fetchAndParse(cacheUrl, perfTimes, parseCache);
+//   postMessage(result);
+//   if (debug) inspectWorker('paralog', result[0]);
+// });
