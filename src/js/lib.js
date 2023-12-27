74161a035b7d605d66db671b99c8e373d30ca584
Eric Weitz <eric.m.weitz@gmail.com>
2023-12-27 12:32:32 -0500

    Refine contrast in boxplot median line

--- src/js/lib.js
+++ src/js/lib.js
@@ -467,18 +467,46 @@ export function ensureContrast(color, bgColor='#FFF') {
     }
   } else {
     const bgRgb = hexToRgb(bgColor);
-    let bgDarkness = 0;
-    if (bgRgb.r < 155) bgDarkness += 1;
-    if (bgRgb.g < 155) bgDarkness += 1;
-    if (bgRgb.b < 155) bgDarkness += 1;
-    if (bgDarkness > 1) {
-      color = `rgb(${rgb.r + 150}, ${rgb.g + 150}, ${rgb.b + 150})`;
+    const contrast = getContrast(
+      [rgb.r, rgb.g, rgb.b],
+      [bgRgb.r, bgRgb.g, bgRgb.b]
+    );
+    if (contrast < 3) {
+      color = `rgb(230, 230, 230)`;
     }
   }
 
   return color;
 }
 
+/** https://stackoverflow.com/a/9733420 */
+const RED = 0.2126;
+const GREEN = 0.7152;
+const BLUE = 0.0722;
+
+const GAMMA = 2.4;
+
+/** https://stackoverflow.com/a/9733420 */
+function luminance(r, g, b) {
+  var a = [r, g, b].map((v) => {
+    v /= 255;
+    return (
+      v <= 0.03928
+        ? v / 12.92
+        : Math.pow((v + 0.055) / 1.055, GAMMA)
+    );
+  });
+  return a[0] * RED + a[1] * GREEN + a[2] * BLUE;
+}
+
+/** https://stackoverflow.com/a/9733420 */
+function getContrast(rgb1, rgb2) {
+  var lum1 = luminance(...rgb1);
+  var lum2 = luminance(...rgb2);
+  var brightest = Math.max(lum1, lum2);
+  var darkest = Math.min(lum1, lum2);
+  return (brightest + 0.05) / (darkest + 0.05);
+}
 
 
 export function adjustBrightness(color, brightness) {
