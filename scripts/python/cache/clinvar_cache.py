4a5d9792bb5dcf1e6ad8a149d097a7f53746202c
Eric Weitz <eric.m.weitz@gmail.com>
2024-12-19 09:24:56 -0500

    Refine clinical cache pipeline

--- scripts/python/cache/clinvar_cache.py
+++ scripts/python/cache/clinvar_cache.py
@@ -1,25 +1,39 @@
 import csv
 import gzip
 
+
+clinical_concerns = ['Likely_pathogenic', 'Pathogenic/Likely_pathogenic', 'Pathogenic']
+robust_review_statuses = [
+    'criteria_provided,_multiple_submitters,_no_conflicts',
+    'reviewed_by_expert_panel',
+    'practice_guideline'
+]
+
 def get_is_relevant(fields):
     is_clinical_concern = False
     is_robustly_reviewed = False
     for field in fields:
         [name, value] = field.split('=')
-        if name == 'CLNSIG' and value in ['Pathogenic', 'Likely_pathogenic', 'Pathogenic/Likely_pathogenic']:
+
+        if name == 'CLNSIG' and value in clinical_concerns:
             is_clinical_concern = True
-        if name == 'CLNREVSTAT' and 'multiple_submitters' in value or value == 'reviewed_by_expert_panel':
+
+        if name == 'CLNREVSTAT' and value in robust_review_statuses:
             is_robustly_reviewed = True
 
     return is_clinical_concern and is_robustly_reviewed
 
 def trim_info_fields(fields):
     slim_fields = []
-    slim_names = ['CLNDN', 'CLNREVSTAT', 'CLNSIG', 'CLNVC', 'MC', 'ORIGIN', 'RS']
+    names_to_keep = ['CLNDN', 'CLNREVSTAT', 'CLNSIG', 'CLNVC', 'MC', 'ORIGIN', 'RS']
     for field in fields:
-        name = field.split('=')[0]
-        if name in slim_names:
-            slim_fields.append(field)
+        [name, value] = field.split('=')
+        if name in names_to_keep:
+            if name == 'CLNSIG':
+                value = clinical_concerns.index(value)
+            elif name == 'CLNREVSTAT':
+                value = robust_review_statuses.index(value)
+            slim_fields.append(f"{name}={value}")
     slim_info = ';'.join(slim_fields)
     return slim_info
 
@@ -43,7 +57,7 @@ with open('clinvar_20241215.vcf') as file:
 
 content = '\n'.join(output_rows)
 
-output_path = 'clinvar_pathogenic_and_lp_20241215.vcf'
+output_path = 'clinvar_priority_20241215.vcf'
 with open(output_path, "w") as f:
     f.write(content)
 with gzip.open(f"{output_path}.gz", "wt") as f:
