6557daf33f3830f715e37d7f4687391f3d57d631
Eric Weitz <eric.m.weitz@gmail.com>
2024-12-20 10:54:56 -0500

    Update test of gene structure functionality

--- test/offline/gene-structure.test.js
+++ test/offline/gene-structure.test.js
@@ -22,7 +22,7 @@ describe('Ideogram gene structure functionality', function() {
         const apoeLabel = document.querySelector('#_c18_a3 path');
         apoeLabel.dispatchEvent(new Event('mouseover'));
         const subparts = document.querySelectorAll('rect.subpart');
-        assert.equal(subparts.length, 7); // spliced, without introns
+        assert.equal(subparts.length, 9); // spliced, without introns
         done();
       }, 500);
     }
