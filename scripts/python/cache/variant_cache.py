e292805a90d567f7d2c94be9e9d7954021dbed98
Eric Weitz <eric.m.weitz@gmail.com>
2024-12-21 07:59:02 -0500

    Write gene byte index for variants

--- scripts/python/cache/variant_cache.py
+++ scripts/python/cache/variant_cache.py
@@ -5,8 +5,13 @@ python variant_cache.py
 """
 
 import csv
-import json
 import gzip
+import json
+import sys
+
+# Avoid "field larger than field limit (131072)"
+csv.field_size_limit(sys.maxsize)
+
 
 clinical_concerns = [
     'Likely_pathogenic',
@@ -103,55 +108,163 @@ def trim_info_fields(fields):
     slim_info = '\t'.join(slim_fields)
     return slim_info
 
-output_rows = []
 
-# https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20241215.vcf.gz
-# Source: https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/
-with open('clinvar_20241215.vcf') as file:
-    reader = csv.reader(file, delimiter="\t")
-    for row in reader:
-        if row[0][0] == '#':
+def write_gene_byte_index(filepath):
+    """Write byte-offset index file of variants for a gene
+    """
+    variant_indexes_by_gene = {}
+    variants_by_chromosome = {}
+    genes_path = '../../../dist/data/cache/genes/homo-sapiens-genes.tsv'
+
+    variant_line_offset = 0
+    print('filepath', filepath)
+    with open(filepath) as file:
+        reader = csv.reader(file, delimiter="\t")
+        for (i, row) in enumerate(reader):
+            if row[0][0] == '#':
+                variant_line_offset += 1
+                continue
+            variant = row
+            chromosome = variant[0]
+            bp_position = int(variant[1]) # base-pair position of start of variant
+            if chromosome not in variants_by_chromosome:
+                variants_by_chromosome[chromosome] = []
+            variants_by_chromosome[chromosome].append([bp_position, i])
+
+    print('variants_by_chromosome', variants_by_chromosome)
+
+    with gzip.open(f"{genes_path}.gz", "rt") as file:
+        reader = csv.reader(file, delimiter="\t")
+        for row in reader:
+            if row[0][0] == '#':
+                continue
+
+            # Gene data
+            chromosome = row[0]
+            start = int(row[1])
+            length = int(row[2])
+            gene = row[4]
+            print('gene', gene)
+
+            if chromosome == 'Y':
+                # ClinVar does not represent variants in chromosome Y by default;
+                # see "papu" resources in ClinVar FTP for variants in chrY
+                # pseudoautosomal region (PAR).
+                continue
+
+            if chromosome not in variants_by_chromosome:
+                # ClinVar represents unlocalized sequences like GL000194.1 as
+                # chromosomes, but they're not of interest here.
+                continue
+
+            # For each variant, check if it's in the gene.
+            # If so, add the index of the variant to a list
+            # of variant indexes for the gene
+            variants_in_chromosome = variants_by_chromosome[chromosome]
+            for variant in variants_in_chromosome:
+                variant_bp_position = variant[0]
+                if variant_bp_position >= start and variant_bp_position <= start + length:
+                    variant_index = variant[1]
+                    if gene not in variant_indexes_by_gene:
+                        variant_indexes_by_gene[gene] = []
+                    variant_indexes_by_gene[gene].append(variant_index)
+
+        variant_gene_index_rows = []
+        for gene in variant_indexes_by_gene:
+            row = variant_indexes_by_gene[gene]
+            row.insert(0, gene)
+            variant_gene_index_rows.append(row)
+
+    header = "# line\tbyte_offset"
+    index = [header] # the byte offset for each line in variants.tsv
+    genes = []
+
+    with open(filepath) as file:
+        lines = file.readlines()
+    for line in lines:
+        if line[0] == '#':
             continue
-        # print('row', row)
-        info = row[7]
-        fields = info.split(';')
-        is_relevant = get_is_relevant(fields)
-        if not is_relevant:
+        gene = line.split('\t')[0]
+        genes.append(gene)
+
+    print('genes[0:3]', genes[0:3])
+    print('genes[-3:]', genes[-3:])
+
+    with open(filepath) as file:
+        char = file.read(1)
+        byte_offset = 0
+        while char != "": # end of file
+            if byte_offset % 100_000 == 0:
+                print(f"Lines byte-indexed, so far: {len(index)}")
+            char = file.read(1)
+            if char == "\n":
+                index.append(byte_offset)
+            byte_offset += 1
+            file.seek(byte_offset)
             continue
-        slim_info = trim_info_fields(fields)
-        del row[5:7]
-        row[5] = slim_info
 
-        output_rows.append('\t'.join(row))
+    with open(f"{filepath}.li", "w") as file:
+        file.write("\n".join([str(o) for o in index]))
+    print(f"Lines byte-indexed, total: {len(index)}")
 
-content = '\n'.join(output_rows)
+def cache_variants(output_path):
 
-disease_map = json.dumps(disease_ids_and_names)
-column_names = [
-    '#CHROM', 'POS', 'ID', 'REF', 'ALT',
-    'DISEASE_IDS', 'CLNREVSTAT', 'CLNSIG', 'CLNVC', 'MC', 'ORIGIN', 'RS'
-]
-headers = '\n'.join([
-    '# Cache data on variants related to human health, from NCBI ClinVar',
-    '# Used by Gene Leads, a feature of Ideogram.js.  This is a compact representation of data from:',
-    '# https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20241215.vcf.gz',
-    '# '
-    '# It filters that data to include only the columns below, and only rows',
-    '# where the variant has a clinical significance of at least "Likely_pathogenic"',
-    '# and a review status of at least "criteria_provided,_multiple_submitters,_no_conflicts".',
-    '# ',
-    '# Keys:'
-    '# disease_mondo_ids_and_names = ' + disease_map,
-    '# variant_types = ' + str(variant_types),
-    '# clinical_significances = ' + str(clinical_concerns),
-    '# clinical_review_statuses = ' + str(robust_review_statuses),
-    '# molecular_consequences = ' + str(molecular_consequences),
-    '\t'.join(column_names)
-])
-content = headers + '\n' + content
+    output_rows = []
+
+    # https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20241215.vcf.gz
+    # Source: https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/
+    with open('clinvar_20241215.vcf') as file:
+        reader = csv.reader(file, delimiter="\t")
+        for row in reader:
+            if row[0][0] == '#':
+                continue
+            # print('row', row)
+            info = row[7]
+            fields = info.split(';')
+            is_relevant = get_is_relevant(fields)
+            if not is_relevant:
+                continue
+            slim_info = trim_info_fields(fields)
+            del row[5:7]
+            row[5] = slim_info
+
+            output_rows.append('\t'.join(row))
+
+    content = '\n'.join(output_rows)
+
+    disease_map = json.dumps(disease_ids_and_names)
+    column_names = [
+        '#CHROM', 'POS', 'ID', 'REF', 'ALT',
+        'DISEASE_IDS', 'CLNREVSTAT', 'CLNSIG', 'CLNVC', 'MC', 'ORIGIN', 'RS'
+    ]
+    headers = '\n'.join([
+        '# Cache data on variants related to human health, from NCBI ClinVar',
+        '# Used by Gene Leads, a feature of Ideogram.js.  This is a compact representation of data from:',
+        '# https://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20241215.vcf.gz',
+        '# ',
+        '# It filters that data to include only the columns below, and only rows',
+        '# where the variant has a clinical significance of at least "Likely_pathogenic"',
+        '# and a review status of at least "criteria_provided,_multiple_submitters,_no_conflicts".',
+        '# ',
+        '# Keys:',
+        '# disease_mondo_ids_and_names = ' + disease_map,
+        '# variant_types = ' + str(variant_types),
+        '# clinical_significances = ' + str(clinical_concerns),
+        '# clinical_review_statuses = ' + str(robust_review_statuses),
+        '# molecular_consequences = ' + str(molecular_consequences),
+        '# ',
+        '\t'.join(column_names)
+    ])
+    content = headers + '\n' + content
+
+    with open(output_path, "w") as f:
+        f.write(content)
+    with gzip.open(f"{output_path}.gz", "wt") as f:
+        f.write(content)
+
+    print('Wrote output to: ' + output_path)
 
 output_path = 'variants.tsv'
-with open(output_path, "w") as f:
-    f.write(content)
-with gzip.open(f"{output_path}.gz", "wt") as f:
-    f.write(content)
+
+# cache_variants(output_path)
+write_gene_byte_index(output_path)
